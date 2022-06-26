a68e2c72014813e02e24826fe0f0ed3cb1ee596b
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 00:55:29 +0700

    Add files via upload

---
+++ plugins/tools-trojan.js
@@ -0,0 +1,34 @@
+let handler = async m => m.reply(`
+「 AKUN GRATIS 」
+Argon
+trojan://c3de63d0-f0d5-11ec-8e34-1577c1651679@id-argontj.bypass.id:443/?sni=s10.tiktokcdn.com#Argon
+
+Atha
+trojan://d85b6290-f0d5-11ec-b1bb-1577c1651679@id-atha.bypass.id:443/?sni=s10.tiktokcdn.com#Atha
+
+CTN
+trojan://e832c210-f0d5-11ec-9a19-1577c1651679@id-ctn.bypass.id:443/?sni=s10.tiktokcdn.com#CTN
+
+Herza
+trojan://f49b22f0-f0d5-11ec-a472-1577c1651679@id-herza.bypass.id:443/?sni=s10.tiktokcdn.com#Herza
+
+Kreatif
+trojan://035cb5b0-f0d6-11ec-9d18-1577c1651679@id-ikd.bypass.id:443/?sni=s10.tiktokcdn.com#Kreatif
+
+CBTP
+trojan://215f9560-f0d6-11ec-a042-1577c1651679@id-tifa.bypass.id:443/?sni=s10.tiktokcdn.com#CBTP
+
+Uninet
+trojan://2cc20190-f0d6-11ec-ab5d-1577c1651679@id-uninet.bypass.id:443/?sni=s10.tiktokcdn.com#Uninet
+
+Contabo
+trojan://5b0885a0-f0d6-11ec-873e-1577c1651679@trojan-contabo1.bypass.id:443/?sni=s10.tiktokcdn.com#Contabo
+
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['akungratis','trojan']
+handler.tags = ['tools']
+handler.command = /^(akungratis|trojan)$/i
+
+handler.limit = true
+
+module.exports = handler
