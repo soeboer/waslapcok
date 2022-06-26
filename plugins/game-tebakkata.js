3e489a5e547383f92dc5cd6cd157de234dd88ac0
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 04:52:44 +0700

    Update game-tebakkata.js

--- plugins/game-tebakkata.js
+++ plugins/game-tebakkata.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkata[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/tebakkata', {}, 'APIKEY'))
+    let res = await fetch(global.API('xteam', '/api/fun/susunkata', {}, 'APIKEY'))
     if (res.status !== 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
