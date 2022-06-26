abec23104525f1927e16416bcd5886ca3b921756
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 01:44:43 +0700

    Update game-family100.js

--- plugins/game-family100.js
+++ plugins/game-family100.js
@@ -1,16 +1,14 @@
 let fetch = require('node-fetch')
-let winScore = 500
+let winScore = 2500
 async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
     if (id in this.game) {
-        this.reply(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', this.game[id].msg)
+        this.sendButton(m.chat, 'Masih ada kuis yang belum terjawab di chat ini', footer, 'Nyerah', 'nyerah', { quoted: this.game[id].msg })
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/family100', {}, 'APIKEY'))
-    if (!res.ok) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/family100.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 *Soal:* ${json.soal}
 
@@ -22,7 +20,7 @@ Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' '
     `.trim()
     this.game[id] = {
         id,
-        msg: await this.sendButton(m.chat, caption, author, 'Nyerah', 'nyerah', m),
+        msg: await this.sendButton(m.chat, caption, footer, 'Nyerah', 'nyerah', m),
         ...json,
         terjawab: Array.from(json.jawaban, () => false),
         winScore,
@@ -32,4 +30,4 @@ handler.help = ['family100']
 handler.tags = ['game']
 handler.command = /^family100$/i
 
-module.exports = handler
+export default handler
