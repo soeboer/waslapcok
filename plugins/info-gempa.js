e7d8544fba2d5668a9d93903abd96e5f87d5dfcd
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 23:56:45 +0700

    Update info-gempa.js

--- plugins/info-gempa.js
+++ plugins/info-gempa.js
@@ -1,19 +1,10 @@
 import axios from 'axios'
 
-let handler = async(m, { conn, text }) => {
-        let res = await axios("https://api.burhansyam.com/bot/gempa.json")
-        let json = res.data
-        let url = json.url
-        let jam = json.jam
-        let tanggal = json.tanggal
-        let lokasi = json.lokasi
-        let kedalaman = json.kedalaman
-        let koordinat = json.koordinat
-        let magnitude = json.magnitude
-        let dirasakan = json.dirasakan
-        let potensi = json.potensi
-        conn.sendFile(m.chat, "*tanggal*", author, url, [['jam', 'dirasakan']], m)
-}
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = await axios("https://api.burhansyam.com/bot/gempa.json")
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, "*Siiiuuuuuu*", author, url, [['⚽ tanggal ⚽', `${usedPrefix + command}`]], m)}
 
 handler.help = ['gempa']
 handler.tags = ['info']
