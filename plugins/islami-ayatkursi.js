c9b6ffe61630c658e812ba5c0ad7bf815608d044
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 21:59:41 +0700

    Update and rename quran-ayatkursi.js to islami-ayatkursi.js

--- plugins/islami-ayatkursi.js
+++ plugins/islami-ayatkursi.js
@@ -12,7 +12,7 @@ Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mere
   m.reply(caption)
 }
 handler.help = ['ayatkursi']
-handler.tags = ['quran']
+handler.tags = ['islami']
 handler.command = /^(ayatkursi)$/i
 
 
