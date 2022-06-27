00714eee47edbe293e53f8c2b45f106dc0eba304
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 22:01:13 +0700

    Update and rename quran-kisahnabi.js to islami-kisahnabi.js

--- plugins/islami-kisahnabi.js
+++ plugins/islami-kisahnabi.js
@@ -6,7 +6,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
      let hasil = `Nabi : ${kisah.name}\nTanggal Lahir : ${kisah.thn_kelahiran}\nTempat Lahir : ${kisah.tmp}\nUsia : ${kisah.usia}\nKisah : ${kisah.description}`
      conn.reply(m.chat, hasil, m)
      }
-handler.help = ['kisahnabi <name>']
-handler.tags = ['quran']
+handler.help = ['kisahnabi <nama nabi>']
+handler.tags = ['islami']
 handler.command = /^kisahnabi$/i
 export default handler
