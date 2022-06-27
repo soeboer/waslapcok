65002a779a6edea281799214a21dd5bbf0dfa715
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 04:02:46 +0700

    Update fun-bucin.js

--- plugins/fun-bucin.js
+++ plugins/fun-bucin.js
@@ -4,7 +4,7 @@ let res = await axios("https://api.burhansyam.com/bot/bucin.json")
 let json = res.data
 let bucin = json.bucin
 let author = json.author
-conn.sendButton(m.chat, "*Quotes Bucin*", author, bucin, [['😍🥰 Lanjut Bucin 😘🤩', `${usedPrefix + command}`]], m)}
+conn.sendButton(m.chat, "*Quotes Bucin*", bucin, author, [['😍🥰 Lanjut Bucin 😘🤩', `${usedPrefix + command}`]], m)}
 handler.help = ['bucin']
 handler.tags = ['fun']
 handler.command = /^(bucin)$/i
