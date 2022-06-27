00d2ee048bb11d6c099cd6e62d49f5867e4a36f8
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 15:25:25 +0700

    Update internet-gempa.js

--- plugins/internet-gempa.js
+++ plugins/internet-gempa.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text }) => {
 await m.reply(global.wait)
     let res = await (await fetch(`https://cililitan.herokuapp.com/api/infogempa`)).json()
         let str = `*INFO GEMPA*\n\nLokasi : ${res.lokasi}\nKedalaman : ${res.kedalaman}\nKoordinat : ${res.koordinat}\nMagnitude : ${res.magnitude}\nPotensi : ${res.potensi}\nWaktu : ${res.waktu}`
-    conn.sendButton(m.chat, await (await fetch(res.gambar)).buffer(), str, footer, 'BMKG', '#? Update', m)
+    conn.sendButton(m.chat, await (await fetch(res.map)).buffer(), str, footer, 'BMKG', '#? Update', m)
 }
 
 handler.help = ['infobmkg']
