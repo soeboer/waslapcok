553032f57fec0c8523fc944f44ead323f70df4b2
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 00:45:42 +0700

    Update info-gempa.js

--- plugins/info-gempa.js
+++ plugins/info-gempa.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let handler = async (m, { conn, text }) => {
-let res = await axios("https://api.burhansyam.com/bot/gempa.json"")
+let res = await axios("https://api.burhansyam.com/bot/gempa.json")
 let json = res.data
 let url = json.url
 conn.sendFile(m.chat, "*Info Gempa BMKG*", url, '', author, m)
