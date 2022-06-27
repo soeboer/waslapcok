f81a8da3a52ef50cc57a690e4900f9e714e1c1fe
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 05:24:42 +0700

    Update internet-masak.js

--- plugins/internet-masak.js
+++ plugins/internet-masak.js
@@ -6,16 +6,16 @@ let res = await fetch(global.API('https://masak-apa.tomorisakura.vercel.app', '/
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { title, thumb, key, times, serving, difficulty } = json.results[0]
-let res2 = await fetch(global.API('https://masak-apa.tomorisakura.vercel.app', '/api/recipe/' {${key}))
-if (!res2.ok) throw await res2.text()
-let json = await res2.json()
-let { title, thumb, times, serving, difficulty, desc, step } = json.results[0]
+// let res2 = await fetch(global.API('https://masak-apa.tomorisakura.vercel.app', '/api/recipe/' {${key}))
+// if (!res2.ok) throw await res2.text()
+// let json = await res2.json()
+// let { title, thumb, times, serving, difficulty, desc, step } = json.results[0]
 let madang = `✨ *Judul :* ${title}
 🎆 *Durasi :* ${times}
 💬 *Hasil :* ${serving}
 💌 *Level :* ${difficulty}
-❤️ *Penyajian :* ${serving}
-👥 *Proses :* ${step}`
+❤️ *Penyajian :* ${serving}`
+// 👥 *Proses :* ${step}
 conn.sendFile(m.chat, thumb, '', madang, m)
 }
 
