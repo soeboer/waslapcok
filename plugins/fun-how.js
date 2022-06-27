76414a1753b72229193d5cbc70e4b11710d47d64
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 12:25:07 +0700

    Update fun-how.js

--- plugins/fun-how.js
+++ plugins/fun-how.js
@@ -7,8 +7,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
         mentions: m.mentionedJid
     } : {})
 }
-handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
-handler.tags = ['kerang', 'fun']
-handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
+handler.help = ['pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
+handler.tags = ['fun']
+handler.command = /^how(pintar|cantik|ganteng|gabut|gila|stress?|bucin|jones|sadboy)/i
 
 export default handler
