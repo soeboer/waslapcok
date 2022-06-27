708516b389a6587242643266a52a834b3dd49d09
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 04:38:34 +0700

    Update internet-masak.js

--- plugins/internet-masak.js
+++ plugins/internet-masak.js
@@ -1,23 +1,20 @@
-let fetch = require("node-fetch")
-
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
 let handler = async (m, { conn, text }) => {
-  let res = await fetch(global.API('zenzapis', '/searching/bacaresep', { query : text }, 'apikey'))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.title) throw json
-  await conn.sendFile(m.chat, json.thumb, '', `
-${json.title}
-${json.url}
-*Judul:* ${json.judul}
-*Tingkat:* ${json.tingkat_kesulitan}
-*Durasi:* ${json.waktu_masak}
-*Porsi:* ${json.hasil}
-
-
-*Bahan:* ${json.bahan}
-*Cara:* ${json.langkah_langkah}
-`.trim(), m)
+if (!text) throw `*[❗INFO❗] Masukan Nama Resep Ingin Kamu Cari*`
+let res = await fetch(global.API('https://api.burhansyam.com', '/bot/resep/', { q: text }))
+if (!res.ok) throw await res.text()
+let json = await res.json()
+let { judul, waktu_masak, hasil, tingkat_kesulitan, thumb, bahan, langkah_langkah } = json.results[0]
+let madang = `✨ *Judul :* ${judul}
+🎆 *Durasi :* ${waktu_masak}
+💬 *Hasil :* ${hasil}
+💌 *Level :* ${tingkat_kesulitan}
+❤️ *Bahan :* ${bahan}
+👥 *Proses :* ${langkah_langkah}`
+conn.sendFile(m.chat, thumb, '', madang, m)
 }
+
 handler.help = ['resep <makanan>', 'masak <makanan>']
 handler.tags = ['internet']
 handler.command = /^(resep|masak)$/i
