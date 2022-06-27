b3a933024b99b1b7ec4350f3be72df4d02b71f3f
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 14:28:36 +0700

    Update game-caklontong.js

--- plugins/game-caklontong.js
+++ plugins/game-caklontong.js
@@ -5,7 +5,7 @@ let poin = 4999
 let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
-    if (id in conn.caklontong) return conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.caklontong[id][0])
+    if (id in conn.caklontong) return conn.reply(m.chat, 'Masih ada pertanyaan yang belum terjawab di chat ini', conn.caklontong[id][0])
     let json = await caklontong()
     let caption = `
 ${json.soal}
@@ -17,7 +17,7 @@ Bonus: ${poin} XP
         await conn.sendButton(m.chat, caption, author, null, [['HELP', `/calo`]], m),
         json, poin,
         setTimeout(async () => {
-            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Time is up!\nThe answer is *${json.jawaban}*\n${json.deskripsi}`, author, null, [['CAKE', `/caklontong`]], conn.caklontong[id][0])
+            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu Habis!\nJawabannya Adaaaaalaaah *${json.jawaban}*\n${json.deskripsi}`, author, null, [['CAKE', `/caklontong`]], conn.caklontong[id][0])
             delete conn.caklontong[id]
         }, timeout)
     ]
