b3348fb280da848d26893f4238d73925aebcf519
Burhan Syam <burhansyam85@gmail.com>
2022-06-27 22:54:22 +0700

    Update info-gempa.js

--- plugins/info-gempa.js
+++ plugins/info-gempa.js
@@ -3,7 +3,7 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
 await m.reply(global.wait)
         let res = await axios("https://api.burhansyam.com/bot/gempa.json")
-        let str = `*INFO GEMPA*\n\nLokasi : ${res.lokasi}\nKedalaman : ${res.kedalaman}\nKoordinat : ${res.koordinat}\nMagnitude : ${res.magnitude}\nPotensi : ${res.potensi}\nWaktu : ${res.waktu}`
+        let str = `*INFO GEMPA*\nTanggal : ${res.tanggal}\nWaktu : ${res.waktu}\nLokasi : ${res.lokasi}\nKedalaman : ${res.kedalaman}\nKoordinat : ${res.koordinat}\nMagnitude : ${res.magnitude}\nPotensi : ${res.potensi}\nDirasakan : ${res.dirasakan}`
         let url = `${res.url}`
         conn.sendFile(m.chat, url, '', str, m)
 }
