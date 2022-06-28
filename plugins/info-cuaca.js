10f75b68a29895fc0d476ca6617795fa5ef216f1
Burhan Syam <burhansyam85@gmail.com>
2022-06-29 06:31:46 +0700

    Update info-cuaca.js

--- plugins/info-cuaca.js
+++ plugins/info-cuaca.js
@@ -1,20 +1,34 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text }) => {
-if (!text) throw 'Masukkan Nama Kota'
-let res = await fetch(global.API('https://api.burhansyam.com', '/bot/cuaca/', { q: text }))
-if (!res.ok) throw await res.text()
-let json = await res.json()
-let { name, weather[0].description, main.temp, main.temp } = json.results
-let madang = `✨ *Kota :* ${name}
-🎆 *Deskrip :* ${weather[0].description}
-👥 *Suhu :* ${main.temp}`
-
-conn.reply(m.chat, 'madang', m)
-}
-
+import axios from 'axios'
 
 handler.help = ['cuaca <kota>']
 handler.tags = ['info']
 handler.command = /^cuaca$/i
+
 export default handler
+
+
+async execute(m) {
+let {conn, text, args} = data
+if(!args[0]) return m.reply("please provide place or location name")
+
+try{
+
+        const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
+        const res = await response
+        
+        const name = res.data.name
+        const Country = res.data.sys.country
+        const Weather= res.data.weather[0].description
+        const Temperature = res.data.main.temp + '°C'
+        const Minimum_Temperature= res.data.main.temp_min + '°C'
+        const Maximum_Temperature= res.data.main.temp_max + '°C'
+        const Humidity= res.data.main.temp + '%'
+        const Wind= res.data.wind.speed + 'km/h'
+        conn.reply(m.chat,`🌸 Place: ${name}\n💮 Country: ${Country}\n🌈 Weather: ${Weather}\n🎋 Temperature: ${Temperature}\n💠 Minimum Temperature: ${Minimum_Temperature}\n📛 Maximum Temperature: ${Maximum_Temperature}\n💦 Humidity: ${Humidity}\n🎐 Wind: ${Wind}
+        `.trim(),m)
+    } catch(e) {
+m.reply('location not found')
+console.log(e)
+    }
+}
+}
