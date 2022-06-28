759927e16f3efcd593ece1e4ddcfbfd078db4d14
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 18:09:12 +0700

    Update tools-spamcall.js

--- plugins/tools-spamcall.js
+++ plugins/tools-spamcall.js
@@ -1,4 +1,10 @@
 let fetch = require('node-fetch')
+
+handler.help = ['spamcall <nomor>']
+handler.tags = ['tools']
+handler.command = /^(spamcall)$/i
+
+export default handler
 let handler = async (m, { conn, text, usedPrefix }) => {
   if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
   let nomor = text.replace(/[^0-9]/gi, '').slice(2)
@@ -10,8 +16,4 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   m.reply(anu)
 }
 // by Aine
-handler.help = ['spamcall <nomor>']
-handler.tags = ['tools']
-handler.command = /^(spamcall)$/i
 
-export default handler
