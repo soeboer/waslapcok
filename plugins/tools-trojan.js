fbf9ecfa9847529112ba12150bed0f6bd232ba6a
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 13:20:36 +0700

    Update tools-trojan.js

--- plugins/tools-trojan.js
+++ plugins/tools-trojan.js
@@ -2,22 +2,28 @@ let handler = async m => m.reply(`
 「 INJEK TELKOMSEX EXTRA UNLI 」
 APN : home | internet | internux
 SNI : m.joox.co | m.spotify.com
+INJEK : all TCP&WS (ssh,ovpn,trojan,vmess)
 
 APN : aha | www.xlgprs.net | xlunlimited
 SNI : s10.tiktokcdn.com | api2-t3.musical.ly
+INJEK : all TCP&WS (ssh,ovpn,trojan,vmess)
 
 「 INJEK TELKOMSEX GAME MAX 30D 10GB 25rb」
 APN : home | internet | internux | aha | www.xlgprs.net | xlunlimited
 SNI : cf-vod.nimotv
-METODE : websocket | bolak balik host & sni
+INJEK : Websocket (ssh-ws,vmess)
+METODE : bolak balik host & sni
 
 「 INJEK TELKOMSEX RUANG GURU 7D 28GB 10RB 」
 APN : home | internet | internux | aha | www.xlgprs.net | xlunlimited
 SNI/IP : 104.18.2.2 | 104.18.2.3
-METODE : websocket | bolak balik host & sni
+INJEK : Websocket (ssh-ws,vmess)
+METODE : bolak balik host & sni
 
+「 AKUN VMESS PREMIUM 」
+vmess://eyJhZGQiOiJpZC12MnJheS5ieXBhc3MuaWQiLCJhaWQiOjAsImlkIjoiOGU4NmIxMGQtMjIzMC00NDExLTkzMTQtMzNiZDQwZDBjOTdkIiwiaG9zdCI6InMxMC50aWt0b2tjZG4uY29tIiwibmV0Ijoid3MiLCJwYXRoIjoiL2Zhc3Rzc2gvcHJlXzUzODk0MjU5OTUvc2VtYW5nYXQ0NS8iLCJwb3J0IjoiNDQzIiwicHMiOiJpZC12MnJheS5ieXBhc3MuaWQiLCJ0bHMiOiJ0bHMiLCJ0eXBlIjoibm9uZSIsInYiOiIyIn0=
 
-「 AKUN TROJAN PREMIUM 」
+「 AKUN TROJAN TCP PREMIUM 」
 Kreatif
 trojan://035cb5b0-f0d6-11ec-9d18-1577c1651679@id-ikd.bypass.id:443/?sni=s10.tiktokcdn.com#Kreatif
 
