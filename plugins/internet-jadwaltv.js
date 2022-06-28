fd995ef95da49829c192fff8b8690d676f87b526
Burhan Syam <burhansyam85@gmail.com>
2022-06-28 12:47:07 +0700

    Update internet-jadwaltv.js

--- plugins/internet-jadwaltv.js
+++ plugins/internet-jadwaltv.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 
 let handler = async (m, { text }) => {
-	if (!text) throw 'Input Query'
+	if (!text) throw 'Masukkan Nama Channel TV'
 	let res = await jadwalTV(text)
 	let txt = res.result.map((v) => `[${v.jam.replace('WIB', ' WIB')}] ${v.acara}`).join`\n`
 	m.reply(`Jadwal TV ${res.channel}\n\n${txt}`)
