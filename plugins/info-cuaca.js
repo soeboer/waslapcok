22dbf1c7fed98878eaf09ff293236f85ff428741
Burhan Syam <burhansyam85@gmail.com>
2022-06-29 13:47:32 +0700

    Update info-cuaca.js

--- plugins/info-cuaca.js
+++ plugins/info-cuaca.js
@@ -1,8 +1,6 @@
 import fetch from 'node-fetch'
 import axios from 'axios'
 
-
-
 let handler = async (m, { conn, text }) => {
 if (!text) throw `*Masukan Nama Kota*`
 let res = await fetch(global.API('https://api.burhansyam.com', '/bot/cuaca/', { q: text }))
@@ -10,10 +8,9 @@ if (!res.ok) throw await res.text()
 let json = await res.json()
 let { name, main.humidity, thumb } = json.results[0]
 
-let madang = `
-✨ *Kota :* ${name}
-🎆 *B :* ${main.humidity}
-`
+let madang = `✨ *Kota :* ${name}
+🎆 *B :* ${main.humidity}`
+
 conn.sendFile(m.chat, thumb, '', madang, m)
 }
 
