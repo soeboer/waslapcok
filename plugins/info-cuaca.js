ef139923a971343e3a8dc28cfedf7e8825f4fc2e
Burhan Syam <burhansyam85@gmail.com>
2022-06-29 13:26:41 +0700

    Update info-cuaca.js

--- plugins/info-cuaca.js
+++ plugins/info-cuaca.js
@@ -1,13 +1,17 @@
 import fetch from 'node-fetch'
-import cheerio from 'cheerio'
+
 let handler = async (m, { conn, text }) => {
-if (!text) throw `*[❗INFO❗] Masukan Nama Kota*`
+if (!text) throw `*Masukan Nama Kota*`
 let res = await fetch(global.API('https://api.burhansyam.com', '/bot/cuaca/', { q: text }))
 if (!res.ok) throw await res.text()
 let json = await res.json()
-let { name, base, thumb } = json.results[0]
-let madang = `✨ *Kota :* ${name}
-🎆 *Umum :* ${base}`
+let { name, main.temp, main[humidity], thumb } = json.results[0]
+
+let madang = `
+✨ *Kota :* ${name}
+🎆 *A :* ${main.temp}
+🎆 *B :* ${main[humidity]}
+`
 conn.sendFile(m.chat, thumb, '', madang, m)
 }
 
