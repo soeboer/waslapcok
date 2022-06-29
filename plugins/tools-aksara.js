6553ea2ed0974b701e15f27e28bd69573d215771
Burhan Syam <burhansyam85@gmail.com>
2022-06-29 17:31:04 +0700

    Update tools-aksara.js

--- plugins/tools-aksara.js
+++ plugins/tools-aksara.js
@@ -1,20 +1,12 @@
 import { latinToAksara } from '@bochilteam/scraper'
 
-// let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-
-//     if (!text) throw `.aksara latinToAksara${usedPrefix}${command} Tulisan`
-//     const result = await latinToAksara(text)
-    
-// await conn.sendButton(m.chat, result, wm, [['Menu', '.menu']], m)
-// }
-
 let handler = async (m, { text, usedPrefix, command }) => {
     if (!text) throw `Example use ${usedPrefix}${command} halo`
     const res = await latinToAksara(text)
     m.reply(`${res}`.trim())
 }
 
-handler.help = ['aksara'].map(v => v + ' <opsi> <teks>')
+handler.help = ['aksara <teks>']
 handler.tags = ['tools']
 handler.command = /^aksara$/i
 
