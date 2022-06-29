fa2b4f5198c8446879e8fbd23dfe9e9e3c6517d0
Burhan Syam <burhansyam85@gmail.com>
2022-06-30 00:54:28 +0700

    Update sticker-smeme.js

--- plugins/sticker-smeme.js
+++ plugins/sticker-smeme.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (stiker) await conn.sendFile(m.chat, stiker, '', author, m, '', { asSticker: 1 })
 }
 handler.help = ['smeme <teks atas>|<teks bawah>']
-handler.tags = ['tools']
+handler.tags = ['stiker']
 handler.command = /^(smeme)$/i
 
 
