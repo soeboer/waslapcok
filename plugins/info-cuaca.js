fd663e048081b353934c640d121f21286c3830f3
Burhan Syam <burhansyam85@gmail.com>
2022-06-29 15:56:37 +0700

    Update info-cuaca.js

--- plugins/info-cuaca.js
+++ plugins/info-cuaca.js
@@ -6,12 +6,17 @@ if (!text) throw `*Masukan Nama Kota*`
 let res = await fetch(global.API('https://api.burhansyam.com', '/bot/cuaca/', { q: text }))
 if (!res.ok) throw await res.text()
 let json = await res.json()
-let { name, main, thumb } = json.results[0]
+let { kota, suhu, cuaca, kelembaban, angin, gbr } = json.results[0]
 
-let madang = `✨ *Kota :* ${name}
-🎆 *B :* ${main}`
+let madang = `
+✨ *Kota :* ${kota}
+✨ *Kota :* ${cuaca}
+🎆 *Suhu :* ${suhu}
+🎆 *Suhu :* ${kelembaban}
+🎆 *Suhu :* ${angin}
+`
 
-conn.sendFile(m.chat, thumb, '', madang, m)
+conn.sendFile(m.chat, gbr, '', madang, m)
 }
 
 handler.help = ['cuaca <kota>']
