0ea88d4f4cde0141cc8dd29bc5cc3dfd96261c09
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 04:10:49 +0700

    Update fun-nomorhoki.js

--- plugins/fun-nomorhoki.js
+++ plugins/fun-nomorhoki.js
@@ -1,12 +1,28 @@
 import { nomorhoki } from '@bochilteam/scraper'
-
-let handler = async (m, { command }) => {
-//     if (!text) throw `Example use ${usedPrefix}${command} halo`
-    const res = await nomorhoki
-    m.reply(`${res}`.trim())
+//let jimp = require('jimp')
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+    //let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
+    if (!text) throw `Contoh Penggunaan ${usedPrefix}${command} 6281xxx`
+    const result = await nomorhoki(text)
+    
+let nohoki = `
+*NOMOR:* ${result.nomer}
+*ANGKA BAGUA SHUZI:* ${result.angka_bagua_shuzi}
+*POSITIF:* ${result.positif.positif}
+*KEKAYAAN:* ${result.positif.kekayaan}
+*KESEHATAN:* ${result.positif.kesehatan}
+*CINTA:* ${result.positif.cinta}
+*KESTABILAN:* ${result.positif.kestabilan}
+*NEGATIF:* ${result.negatif.negatif}
+*PERSELISIHAN:* ${result.negatif.perselisihan}
+*KEHILANGAN:* ${result.negatif.kehilangan}
+*MALAPETAKA:* ${result.negatif.malapetaka}
+*KEHANCURAN:* ${result.negatif.Kehancuran}
+`
+await conn.sendButton(m.chat, nohoki, wm, [['menu', '.menu']], m)
 }
 
-handler.help = ['nomorhoki']
+handler.help = ['nomorhoki'].map(v => v + ' <Apa>')
 handler.tags = ['fun']
 handler.command = /^(nomorhoki)$/i
 
