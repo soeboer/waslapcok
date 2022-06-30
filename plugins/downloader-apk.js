cfaf29780bca1202574de681df6244b568e29dce
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 04:16:48 +0700

    Update downloader-apk.js

--- plugins/downloader-apk.js
+++ plugins/downloader-apk.js
@@ -1,27 +1,133 @@
-import cheerio from 'cheerio'
 import fetch from 'node-fetch'
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-	if (!args[0]) throw `Ex: ${usedPrefix + command} https://play.google.com/store/apps/details?id=com.linecorp.LGGRTHN`
-	let res = await apkDl(args[0])
-	await m.reply('_In progress, please wait..._')
-	conn.sendMessage(m.chat, { document: { url: res.download }, mimetype: res.mimetype, fileName: res.fileName }, { quoted: m })
+import fs from 'fs'
+let handler = async (m, {text, usedPrefix, command, conn}) => {
+let frep = { contextInfo: { externalAdReply: {title: global.wm, body: 'Simple Bot Esm', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}}
+  if (!text) throw `Contoh penggunaan ${usedPrefix}${command} Minecraft`
+  
+  if (command == 'apkdone') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/apkdone?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+Version : ${x.apps_version}
+Rate : ${x.apps_rate}
+Tag : ${x.apps_tag}
+`}
+        await conn.reply(m.chat, caption, m, frep)
 }
-handler.help = handler.alias = ['apkdl']
+
+if (command == 'apkgoogle') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/apkgoogle?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+Tag : ${x.apps_tag}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'apkmody') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/apkmody?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+Desc : ${x.desc}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'apkshub') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/apkshub?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+View : ${x.apps_views}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'happymod') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/happymod?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+Rate : ${x.apps_rate}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'hostapk') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/hostapk?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+Rilis : ${x.apps_released}
+Athor : ${x.apps_author}
+Desc : ${x.apps_desc}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'revdl') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/revdl?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'toraccino') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/toraccino?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+Tag : ${x.apps_tag}
+Rilis : ${x.apps_upload}
+Athor : ${x.apps_author}
+Desc : ${x.apps_desc}
+`}
+        await conn.reply(m.chat, caption, m, frep)
+}
+
+if (command == 'uapkpro') {
+let json = await fetch(`https://dhn-api.herokuapp.com/api/apk/uapkpro?apps=${text}&apikey=4ca83deeb14d61d16cf0`)
+        let jsons = await json.json()
+        let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+        for (let x of jsons.result) {
+        caption += `
+🤠 *Name* : *${x.apps_name}*
+Link : ${x.apps_linkdl}
+`}
+        await conn.reply(m.chat, frep, caption, m)
+}
+
+}
+handler.command = handler.help = ['apkdone', 'apkgoogle', 'apkmody', 'apkshub', 'happymod', 'hostapk', 'revdl', 'toraccino', 'uapkpro']
 handler.tags = ['downloader']
-handler.command = /^(apkdl)$/i
 
 export default handler
-
-async function apkDl(url) {
-	let res = await fetch('https://apk.support/gapi/index.php', {
-		method: 'post',
-		body: new URLSearchParams(Object.entries({ x: 'downapk', t: 1, google_id: url, device_id: '', language: 'en-US', dpi: 480, gl: 'SUQ=', model: '', hl: 'en', de_av: '', aav: '', android_ver: 5.1, tbi: 'arm64-v8a', country: 0, gc: undefined }))
-	})
-	let $ = cheerio.load(await res.text())
-	let fileName = $('div.browser > div.dvContents > ul > li > a').text().trim().split(' ')[0]
-	let download = $('div.browser > div.dvContents > ul > li > a').attr('href')
-	if (!download) throw 'Can\'t download the apk!'
-	let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type')
-	return { fileName, mimetype, download }
-}
