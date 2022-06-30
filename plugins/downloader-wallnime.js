fc4dd05e5447e793cc716b286b78a8b9cf8626c3
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 06:27:24 +0700

    Update downloader-wallnime.js

--- plugins/downloader-wallnime.js
+++ plugins/downloader-wallnime.js
@@ -1,12 +1,11 @@
 let handler = async (m, {command, conn}) => {
-    if (command == 'nsfwloli') {
-        m.reply('zim bot processing')
+        m.reply('bot processing')
         let haha = await conn.getFile(`https://hadi-api.herokuapp.com/api/walpaperanime`)
-        conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['NEXT', `/${command}`]], m)
+        conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['😎 Acak 😋', `/${command}`]], m)
     } 
-}
 
-handler.help = ['nsfwloli', 'nsfwahegao', 'nsfwass', 'nsfwbdsm', 'nsfwblowjob', 'nsfwcuckold', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwgangbanh', 'nsfwglasses', 'nsfwhentai', 'nsfwjahy', 'nsfwmanga', 'nsfwmstb', 'nsfwneko', 'nsfworgy', 'nsfwpanties', 'nsfwpussy', 'nsfwneko2', 'nsfwtentacles', 'nsfwthings', 'nsfwyuri', 'nsfwzettai']
-handler.command = ['nsfwloli', 'nsfwahegao', 'nsfwass', 'nsfwbdsm', 'nsfwblowjob', 'nsfwcuckold', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwgangbanh', 'nsfwglasses', 'nsfwhentai', 'nsfwjahy', 'nsfwmanga', 'nsfwmstb', 'nsfwneko', 'nsfworgy', 'nsfwpanties', 'nsfwpussy', 'nsfwneko2', 'nsfwtentacles', 'nsfwthings', 'nsfwyuri', 'nsfwzettai']
+}
+handler.help = ['random wallpaper anime']
+handler.command = ['wallnime']
 handler.tags = ['downloader']
 export default  handler
