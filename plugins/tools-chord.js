20b816be76702d9d87bf58b27b3434dca2144a7c
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 05:23:31 +0700

    Update tools-chord.js

--- plugins/tools-chord.js
+++ plugins/tools-chord.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let handler = async(m, { conn, text, usedPrefix }) => {
 
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'chord hanya rindu', m)
-    axios.get(`https://docs-jojo.herokuapp.com/api/chord?q=` + text)
+    axios.get(`https://hadi-api.herokuapp.com/api/chord/?q=` + text)
         .then((res) => {
           let hasil = `*• Chord Lagu ${text} :*\n${res.data.result}`
             conn.reply(m.chat, hasil, m)
