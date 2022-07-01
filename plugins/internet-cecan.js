892246eb007e0a7968cad869b040c372c88fb45c
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 07:23:39 +0700

    Update internet-cecan.js

--- plugins/internet-cecan.js
+++ plugins/internet-cecan.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 await m.reply('dalam proses...')
 let img = await conn.getFile(`https://hadi-api.herokuapp.com/api/randomImage/cecan`)
 var capt = `🐦 Random Foto Cecan🗿`
-conn.sendButton(m.chat, capt, wm, img, [['🤗 Acak 😋', `.cecan`]], m)
+conn.sendButton(m.chat, capt, wm, img.jpg, [['🤗 Acak 😋', `.cecan`]], m)
 }
 
 handler.help = ['wallpaper Cecan']
