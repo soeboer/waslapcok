c1fb06b6b47598057cecaa9cca8d2d03998e2797
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 18:37:07 +0700

    Update internet-cecan.js

--- plugins/internet-cecan.js
+++ plugins/internet-cecan.js
@@ -5,7 +5,7 @@ var capt = `🐦 Random Foto Cecan🗿`
         conn.sendButton(m.chat, `_${command}_`.trim(), capt, img.data, [['😋 acak 🤗', `/${command}`]], m)
 }
 
-handler.help = ['wallpaper Cecan']
+handler.help = ['igo Cecan']
 handler.tags = ['downloader']
 
 handler.command = /^(igo|cecan)$/i
