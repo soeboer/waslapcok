1e9da481d61d9db981d0c11f98765a16877de172
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 00:15:51 +0700

    Update downloader-sound.js

--- plugins/downloader-sound.js
+++ plugins/downloader-sound.js
@@ -1,9 +1,8 @@
 import fetch from 'node-fetch'
+import axios from 'axios'
 
-let handler = async(m, { conn, text }) => {
-
-let listeffect = `
-*List Sound :*
+let handler = async (m, { args, usedPrefix, command }) => {
+    let er = `
 anjay
 ara-ara
 ara-ara-cowok
@@ -74,20 +73,98 @@ woi
 yamete
 yowaimo
 yoyowaimo
-`.trim()
 
-    if (!effect) return conn.reply(m.chat, listeffect, m)
+contoh:
+${usedPrefix + command} daisuki
+    `.trim()
+    if (!args[0]) throw er
 
-  await m.reply('tunggu dulu 🐦')
-//let img = await conn.getFile(`https://violetics.pw/api/asupan/${pilih}?apikey=beta`)
-  
- let audio = await conn.getFile(`https://raw.githubusercontent.com/saipulanuar/Api-Github/main/audio/${listeffect}.mp3`)
- let caption = `*SFX*\n\nEffect : ${listeffect}`
+    switch (args[0].toLowerCase()) {
+case 'anjay':
+case 'ara-ara':
+case 'ara-ara-cowok':
+case 'ara-ara2':
+case 'arigatou':
+case 'assalamualaikum':
+case 'asu':
+case 'ayank':
+case 'aku-ngakak':
+case 'bacot':
+case 'bahagia-aku':
+case 'baka':
+case 'bansos':
+case 'beat-box':
+case 'beat-box2':
+case 'biasalah':
+case 'bidadari':
+case 'bot':
+case 'buka-pintu':
+case 'canda-anjing':
+case 'cepetan':
+case 'cuekin-terus':
+case 'daisuki-dayo':
+case 'daisuki':
+case 'dengan-mu':
+case 'gaboleh-gitu':
+case 'gak-lucu':
+case 'gamau':
+case 'gay':
+case 'gelay':
+case 'gitar':
+case 'gomenasai':
+case 'hai-bot':
+case 'hampa':
+case 'hayo':
+case 'hp-iphone':
+case 'i-like-you':
+case 'ih-wibu':
+case 'india':
+case 'karna-lo-wibu':
+case 'kiss':
+case 'kontol':
+case 'ku-coba':
+case 'maju-wibu':
+case 'makasih':
+case 'mastah':
+case 'nande-nande':
+case 'nani':
+case 'ngadi-ngadi':
+case 'nikah': case 'nuina':
+case 'onichan':
+case 'owner-sange':
+case 'ownerku':
+case 'pak-sapardi':
+case 'pale':
+case 'pantek':
+case 'pasi-pasi':
+case 'punten':
+case 'sayang':
+case 'siapa-sih':
+case 'sudah-biasa':
+case 'summertime':
+case 'tanya-bapak-lu':
+case 'to-the-bone':
+case 'wajib':
+case 'waku':
+case 'woi':
+case 'yamete':
+case 'yowaimo':
+case 'yoyowaimo':
+            let text = args.slice(1).join(' ')
+            let pilih = args[0].toLowerCase()
+await m.reply('dalam proses...')
+let sound = await conn.getFile(`https://raw.githubusercontent.com/saipulanuar/Api-Github/main/audio/${pilih}.mp3`)
+var capt = `🐦 Koleksi Random Foto Video ${pilih} 🗿`
+     conn.sendFile(m.chat, `_${usedPrefix + command} ${pilih}_`.trim(), capt, sound.data, '', m)
 
-// await conn.sendFile(m.chat, audio, 'error.mp3', null, m, true) 
- 
-    conn.sendFile(m.chat, audio, 'error.mp3', caption, m)
+            break
+        default:
+            throw er
+    }
 }
+
+
+
 handler.help = ['sfx']
 handler.tags = ['downloader']
 handler.command = /^(sfx)$/i
