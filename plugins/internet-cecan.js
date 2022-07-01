35b7c0d22dbd27e71c4a737f69e71698bd1f3e5d
Burhan Syam <burhansyam85@gmail.com>
2022-07-01 07:02:51 +0700

    Create internet-cecan.js

---
+++ plugins/internet-cecan.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+await m.reply('dalam proses...')
+let img = await conn.getFile(`https://hadi-api.herokuapp.com/api/randomImage/cecan`)
+var capt = `🐦 Random Foto Cecan🗿`
+conn.sendButton(m.chat, capt, wm, img, [['🤗 Acak 😋', `.cecan`]], m)
+}
+
+handler.help = ['wallpaper Cecan']
+handler.tags = ['downloader']
+
+handler.command = /^(igo|cecan)$/i
+
+export default handler
