3c32f1ed59da13cd9ca76168329cdf7e87ea9e4d
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 02:07:58 +0700

    Update game-tebakkata_ans.js

--- plugins/game-tebakkata_ans.js
+++ plugins/game-tebakkata_ans.js
@@ -2,20 +2,23 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teka/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*calo/i.test(m.quoted.text) || /.*(calo|bantuan)/i.test(m.text))
+        return !0
     this.tebakkata = this.tebakkata ? this.tebakkata : {}
-    if (!(id in this.tebakkata)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.tebakkata)) return m.reply('Waktu habisss')
     if (m.quoted.id == this.tebakkata[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tebakkata[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakkata[id][2]
-            global.db.data.users[m.sender].tiketcoin += 1
-            m.reply(`*Benar!*\n+${this.tebakkata[id][2]} XP\n+1 Tiketcoin`)
+            await this.sendButton(m.chat, `*Betuool!* +${this.tebakkata[id][2]} XP\n${json.deskripsi}`, author, null, [['Tebak Kata', '.tebakkata']], m)
             clearTimeout(this.tebakkata[id][3])
             delete this.tebakkata[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
+            m.reply(`*Sedikit Lagi!*`)
+        else {
+            if (m.text != '/calo') return
+            m.reply(`*Salaaah!*`)
+        }
     }
     return !0
 }
