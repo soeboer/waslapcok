ef20128d0ea2392a395b79ad9d12d8d0dbcea8e0
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 22:47:16 +0700

    Update fun-yasin.js

--- plugins/fun-yasin.js
+++ plugins/fun-yasin.js
@@ -1,13 +1,14 @@
 let handler = async (m, { conn, args }) => {
 // let text = args.slice(1).join(' ')
+   let text = args.join(' ').split('|')
+if (!args[0]) throw 'Tulis Namanya'
 
-if (!args[0]) throw 'Masukkan Parameter'
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   m.reply('Proses...')
 conn.sendFile(m.chat, global.API('https://hadi-api.herokuapp.com', '/api/canvas/yasin', {
 url: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-name: text || '${response[0]}'
-}), 'error.png', `*@${response[0]}*`, m)
+name: text || '${text[0]}'
+}), 'error.png', `*@${text[0]}*`, m)
 }  
 
 
