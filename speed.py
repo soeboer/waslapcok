32b18c413bf6cd3d4053e577f1697f29f156b9c9
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 17:47:16 +0700

    Update speed.py

--- speed.py
+++ speed.py
@@ -1964,7 +1964,7 @@ def shell():
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
-        printer("\n...................................................................................\n▶︎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ *OOKLA*\n▶︎")
+        printer("\n...................................................................................\n▶︎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ *OOKLA*\n▶︎burhansyam was here")
     else:
         printer('Skipping upload test', quiet)
 
