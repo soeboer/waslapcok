4bed3fc3a79522eb87ec350a5e82a1ee0d9a21f1
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 22:24:25 +0700

    Update fun-yasin.js

--- plugins/fun-yasin.js
+++ plugins/fun-yasin.js
@@ -5,8 +5,9 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Masukkan Namanya...'
   m.reply('Proses Membuat Sampul Buku Yasin...')
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let avatar = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-  let res = `https://hadi-api.herokuapp.com/api/canvas/yasin?name=${response[0]}&url=${avatar}`
+    let url = await conn.profilePictureUrl(who, 'image')
+//     let url = await conn.getProfilePicture(conn.user.jid)
+    let res = `https://hadi-api.herokuapp.com/api/canvas/yasin?name=${response[0]}&url=${url}`
   conn.sendFile(m.chat, res, 'yasin.jpg', `Turut Mengenang`, m, false)
 }
  
