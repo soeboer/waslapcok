be0e2764717caeeb0e6246bfc31e7f98a118c35a
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 01:27:17 +0700

    Update game-tebakkata.js

--- plugins/game-tebakkata.js
+++ plugins/game-tebakkata.js
@@ -1,4 +1,5 @@
-import { tebakkata } from '@bochilteam/scraper'
+// import { tebakkata } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
 
 let timeout = 120000
 let poin = 4999
@@ -6,21 +7,23 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
     let id = m.chat
     if (id in conn.tebakkata) {
-        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebakkata[id][0])
+        conn.reply(m.chat, 'Masih ada soal belum terjawab kak,maaf ya...', conn.tebakkata[id][0])
         throw false
     }
-    const json = await tebakkata()
+//     const json = await tebakkata()
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkata.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 ${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Type ${usedPrefix}teka for help
+Balas ${usedPrefix}teka untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkata[id] = [
-        await conn.sendButton(m.chat, caption, author, ['HINT', `${usedPrefix}teka`], m),
+        await conn.sendButton(m.chat, caption, author, ['BANTUAN', `${usedPrefix}teka`], m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakkata[id]) conn.sendButton(m.chat, `Time is up!\nThe answer is *${json.jawaban}*`, author, ['tebakkata', `${usedPrefix}tebakkata`], conn.tebakkata[id][0])
+            if (conn.tebakkata[id]) conn.sendButton(m.chat, `Waktu Habis!\nJawaban yg bear adalah *${json.jawaban}*`, author, ['tebakkata', `${usedPrefix}tebakkata`], conn.tebakkata[id][0])
             delete conn.tebakkata[id]
         }, timeout)
     ]
