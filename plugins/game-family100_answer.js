4eab70d92b93653220d73090d4519d3b96a62338
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 01:51:41 +0700

    Update game-family100_answer.js

--- plugins/game-family100_answer.js
+++ plugins/game-family100_answer.js
@@ -1,43 +1,44 @@
 import similarity from 'similarity'
 const threshold = 0.72 // the higher the value, the more similar
 export async function before(m) {
-    this.game = this.game ? this.game : {}
-    let id = 'family100_' + m.chat
-    if (!(id in this.game))
-        return !0
-    let room = this.game[id]
-    let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
-    let isSurrender = /^((me)?giveup|surr?ender)$/i.test(m.text)
-    if (!isSurrender) {
-        let index = room.jawaban.indexOf(text)
-        if (index < 0) {
-            if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold)
-                m.reply('A little more!')
-            return !0
+//     async before(m) {
+        this.game = this.game ? this.game : {}
+        let id = 'family100_' + m.chat
+        if (!(id in this.game)) return !0
+        let room = this.game[id]
+        let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
+        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
+        if (!isSurrender) {
+            let index = room.jawaban.indexOf(text)
+            if (index < 0) {
+                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
+                return !0
+            }
+            if (room.terjawab[index]) return !0
+            let users = global.db.data.users[m.sender]
+            room.terjawab[index] = m.sender
+            users.money += room.winScore
         }
-        if (room.terjawab[index])
-            return !0
-        let users = global.db.data.users[m.sender]
-        room.terjawab[index] = m.sender
-        users.exp += room.winScore
-    }
-    let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
-    let caption = `
-*Question:* ${room.soal}
-There is *${room.jawaban.length}* answer${room.jawaban.find(v => v.includes(' ')) ? `
-(some answers have spaces)
-` : ''}
-${isWin ? `*ALL ANSWERS*` : isSurrender ? '*SURRENDER!*' : ''}
+        let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
+        let caption = `
+*Soal:* ${room.soal}
+Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
+(beberapa jawaban terdapat spasi)
+`: ''}
+${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
 ${Array.from(room.jawaban, (jawaban, index) => {
-        return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
-    }).filter(v => v).join('\n')}
-${isSurrender ? '' : `+${room.winScore} XP every answer is correct`}
+            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
+        }).filter(v => v).join('\n')}
+${isSurrender ? '' : `+${room.winScore} Money tiap jawaban benar`}
     `.trim()
-    const msg = await this.sendButton(m.chat, caption, author, null, [[`${(isWin || isSurrender) ? 'Family 100' : 'Give up'}`, `${(isWin || isSurrender) ? '.family100' : 'Give up'}`]], null, {
-        mentions: this.parseMention(caption)
-    })
-    room.msg = msg
-    if (isWin || isSurrender)
-        delete this.game[id]
-    return !0
+        m.reply(caption, null, {
+            contextInfo: {
+                mentionedJid: this.parseMention(caption)
+            }
+        }).then(msg => {
+            return this.game[id].msg = msg
+        }).catch(_ => _)
+        if (isWin || isSurrender) delete this.game[id]
+        return !0
+    }
 }
