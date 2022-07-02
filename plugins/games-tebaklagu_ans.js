ee3cc3f085b487799abc7885e10beceb6c9a9386
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 05:38:26 +0700

    Update games-tebaklagu_ans.js

--- plugins/games-tebaklagu_ans.js
+++ plugins/games-tebaklagu_ans.js
@@ -5,14 +5,15 @@ export async function before(m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/TEBAK JUDUL LAGU/i.test(m.quoted.text)) return !0
     this.tebaklagu = this.tebaklagu ? this.tebaklagu : {}
-    if (!(id in this.tebaklagu)) return m.reply('Soal ny telah berakhir kak')
+    if (!(id in this.tebaklagu)) return m.reply('Soal nya telah berakhir kak')
     if (m.quoted.id == this.tebaklagu[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.judul.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
             global.db.data.users[m.sender].tiketcoin += 1
-            m.reply(`*Benar!*\n+${this.tebaklagu[id][2]} XP\n+1 Tiketcoin`)
+//             m.reply(`*Benar!*\n+${this.tebaklagu[id][2]} XP\n+1 Tiketcoin`)
+                        await this.sendButton(m.chat, `*Benar!* +${this.tebaklagu[id][2]} XP\n+1 Tiketcoin`, author, null, [['Main Lagi', '.tebaklagu']], m) 
             clearTimeout(this.tebaklagu[id][3])
             delete this.tebaklagu[id]
         } else if (similarity(m.text.toLowerCase(), json.judul.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
