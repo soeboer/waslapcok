f8ab4f9a647542ca1d813eacaa7cc4438dbeb702
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 17:16:36 +0700

    Update tools-spamcall.js

--- plugins/tools-spamcall.js
+++ plugins/tools-spamcall.js
@@ -1,19 +1,23 @@
 import fetch from 'node-fetch'
 
-handler.help = ['spamcall <nomor>']
-handler.tags = ['tools']
-handler.command = /^(spamcall)$/i
-
-export default handler
 let handler = async (m, { conn, text, usedPrefix }) => {
   if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
   let nomor = text.replace(/[^0-9]/gi, '').slice(2)
   if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
+  
   m.reply('_*Tunggu permintaan anda sedang diproses.....*_')
+  
   let anu = await fetch(`https://id.jagreward.com/member/verify-mobile/${nomor}`).then(a => a.json())
   let spcall = `*Nomor bot* : _${anu.phone_prefix}_\n\n_Bot berhasil menelpon anda!_`
   conn.reply(m.chat, `${spcall}`.trim(), m)
   m.reply(anu)
 }
+
+handler.help = ['spamcall <nomor>']
+handler.tags = ['tools']
+handler.command = /^(spamcall)$/i
+
+
+export default handler
 // by Aine
 
