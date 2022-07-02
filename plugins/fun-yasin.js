1cf0f6a82862834cf2fd59e62d46c21329d00c42
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 22:35:37 +0700

    Update fun-yasin.js

--- plugins/fun-yasin.js
+++ plugins/fun-yasin.js
@@ -3,8 +3,8 @@ let text = args.slice(1).join(' ')
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://hadi-api.herokuapp.com', '/api/canvas/yasin', {
 url: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-text: text || 'Warga WA'
-}), 'error.png', `*@${who}*`, m)
+name: text || 'Warga WA'
+}), 'error.png', `*@${author}*`, m)
 }  
 
 
