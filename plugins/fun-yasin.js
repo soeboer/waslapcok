9754caed2265207c1b7242adc98a2ee71b62ec76
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 22:07:43 +0700

    Update fun-yasin.js

--- plugins/fun-yasin.js
+++ plugins/fun-yasin.js
@@ -1,17 +1,13 @@
 import fetch from 'node-fetch'
 
-let handler = async(m, { conn, text }) => {
-let [effect, teks] = text.split ` `
-let listeffect = `yasin`.trim()
-
-    if (!effect) return conn.reply(m.chat, listeffect, m)
-    if (!teks) return conn.reply(m.chat, 'tulis Namanya?', m)
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let avatar = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-  await m.reply('Sabar Kak saya buatkan dulu...')
- let hasil = await conn.getFile(`https://hadi-api.herokuapp.com/api/canvas/yasin/?name=${teks}?url=${avatar}`)
- let caption = `*Buku Yasin*\n\nUntuk : ${who}`
-    conn.sendFile(m.chat, hasil.data, 'yasin.jpg', caption, m)
+let handler = async (m, { conn, args }) => {
+   let response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Namanya...'
+  m.reply('Proses Membuat Sampul Buku Yasin...')
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let avatar = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+  let res = `https://hadi-api.herokuapp.com/api/canvas/yasin?name=${response[0]}&url=${avatar}`
+  conn.sendFile(m.chat, res, 'yasin.jpg', `Turut Mengenang`, m, false)
 }
  
 handler.help = ['yasin']
