3fa9f14209cb182883754a04ea2e76159d5032ea
Burhan Syam <burhansyam85@gmail.com>
2022-07-02 22:31:05 +0700

    Update fun-yasin.js

--- plugins/fun-yasin.js
+++ plugins/fun-yasin.js
@@ -1,16 +1,13 @@
-import fetch from 'node-fetch'
-
 let handler = async (m, { conn, args }) => {
-   let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Namanya...'
-  m.reply('Proses Membuat Sampul Buku Yasin...')
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let url = await conn.profilePictureUrl(who, 'image')
-//     let url = await conn.getProfilePicture(conn.user.jid)
-    let res = `https://hadi-api.herokuapp.com/api/canvas/yasin?name=${response[0]}&url=${url}`
-  conn.sendFile(m.chat, res, 'yasin.jpg', `Turut Mengenang`, m, false)
-}
- 
+let text = args.slice(1).join(' ')
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+conn.sendFile(m.chat, global.API('https://hadi-api.herokuapp.com', '/api/canvas/yasin', {
+url: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+text: text || 'Warga WA'
+}), 'error.png', `*@${author}*`, m)
+}  
+
+
 handler.help = ['yasin']
 handler.tags = ['fun']
 handler.command = /^(yasin)$/i
