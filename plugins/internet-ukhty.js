8087866a5bd2d1e81b3e064ff94be02ef4053937
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 19:32:58 +0700

    Update internet-ukhty.js

--- plugins/internet-ukhty.js
+++ plugins/internet-ukhty.js
@@ -1,13 +1,10 @@
-import axios from 'axios'
-
 let handler = async (m, {command, conn}) => {
-let res = await axios("https://api.burhansyam.com/bot/ukhty.json")
-let json = res.data
-let ukhty = json.ukhty
-let author = json.author
-// conn.sendButton(m.chat, "_*Quotes Bucin :*_", ukhty, author, [['😍🥰 Lanjut 😘🤩', `${usedPrefix + command}`]], m)}
-       conn.sendButton(m.chat, `_${command}_`.trim(), author, ukhty, [['😋 acak 🤗', `/${command}`]], m)
-  
+await m.reply('Masih saya pantau kak...')
+let img = await conn.getFile(`https://api.burhansyam.com/bot/ukhty.json`)
+var capt = `🐦 Random Foto Ukhty🗿`
+        conn.sendButton(m.chat, `_${command}_`.trim(), capt, img.data, [['😋 acak 🤗', `/${command}`]], m)
+}
+
 handler.help = ['ukhty']
 handler.tags = ['fun']
 handler.command = /^(ukhty)$/i
