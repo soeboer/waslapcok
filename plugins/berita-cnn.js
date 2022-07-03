8835b3b72bcf0218c4af761a47dd088fad968bfa
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 16:30:10 +0700

    Update berita-cnn.js

--- plugins/berita-cnn.js
+++ plugins/berita-cnn.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, {command, conn}) => {
 //      let res = await (await fetch('https://hadi-api.herokuapp.com/api/kompas')).json()
-     let res = await fetch(global.API('https://hadi-api.herokuapp.com', '/api/berita'))
+     let res = await fetch(global.API('https://hadi-api.herokuapp.com', '/api/beritanasional'))
      let json = await res.json()
      let { judul, articleUrl, date_created, image, desc, body } = json.result[0]
 let kompis = `📺 *CNN News*
