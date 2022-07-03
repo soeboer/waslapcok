389b0c92e5893b6344fca0aa4ced7a07b2463ae3
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 15:53:26 +0700

    Create tools-map.js

---
+++ plugins/tools-map.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import imageToBase64 from 'imageToBase64'
+
+await m.reply('Searching...')
+let str = `
+*Hasil Pencarian Daerah :*
+${text}
+`.trim()
+
+    axios.get('https://mnazria.herokuapp.com/api/maps?search=' + text)
+    .then((res) => {
+      imageToBase64(res.data.gambar)
+        .then(
+          (ress) => {
+            let buf = Buffer.from(ress, 'base64')
+
+     conn.sendFile(m.chat, buf, 'peta.jpg', str, m)
+        })
+    })
+}
+handler.help = ['peta <wilayah>']
+handler.tags = ['tools']
+handler.command = /^(peta|map)$/i
+export default handler 
