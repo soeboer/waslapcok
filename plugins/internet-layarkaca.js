c89383e02f716efea96fc026693eece5e53a48a1
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 13:42:02 +0700

    Update internet-layarkaca.js

--- plugins/internet-layarkaca.js
+++ plugins/internet-layarkaca.js
@@ -32,7 +32,7 @@ ${hasil.map(v => `
 `.trim())
 }
 
-handler.help = ['Layarkaca'].map(v => v + ' <query>')
+handler.help = ['lk21'].map(v => v + ' <query>')
 handler.tags = ['internet']
-handler.command = /^(layarkaca)$/i
+handler.command = /^(lk21)$/i
 export default handler
