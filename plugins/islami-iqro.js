e4147239806f125afc872ed7c4bd1145fa137864
Burhan Syam <burhansyam85@gmail.com>
2022-07-04 03:04:35 +0700

    Update islami-iqro.js

--- plugins/islami-iqro.js
+++ plugins/islami-iqro.js
@@ -1,12 +1,27 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  
-let oh = `Contoh : ${usedPrefix + command} 3\n\nIQRA Yang tersedia : 1,2,3,4,5,6`
-		if (!text) throw oh
+let handler = async (m, { args, usedPrefix, command }) => {
 	
-// 		conn.sendFile(m.chat, {document: yy.data, mimetype: 'application/pdf', fileName: `iqro${text}.pdf`}, {quoted:m}).catch ((err) => m.reply(oh))	
-		let hasil = await conn.getFile(`https://islamic-api-indonesia.herokuapp.com/api/data/pdf/iqra${text}`)
-		let caption = `*Iqro*\n\nJilid : ${text}`
-                conn.sendFile(m.chat, hasil.data, `iqro-jilid-${text}.pdf`, caption, m)
+let er = `IQRA Yang tersedia : 1,2,3,4,5,6
+contoh:
+${usedPrefix + command} 3
+    `.trim()
+    if (!args[0]) throw er
+    switch (args[0].toLowerCase()) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':         
+            let text = args.slice(1).join(' ')
+            let pilih = args[0].toLowerCase()
+		await m.reply('ditunggu dulu kak...')	
+		let hasil = await conn.getFile(`https://islamic-api-indonesia.herokuapp.com/api/data/pdf/iqra${pilih}`)
+		let caption = `*Iqro*\n\nJilid : ${pilih}`
+                conn.sendFile(m.chat, hasil.data, `iqro-jilid-${pilih}.pdf`, caption, m)
+	break
+        default:
+            throw er
+    }
 		}
 handler.help = ['iqro']
 handler.tags = ['islami']
