8aa4aa017c234ea58365eae3a79b9a3e1c9aca57
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 16:17:02 +0700

    Update and rename berita-cnn.js to berita-detik.js

---
+++ plugins/berita-detik.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+
+let handler = async (m, {command, conn}) => {
+//      let res = await (await fetch('https://hadi-api.herokuapp.com/api/kompas')).json()
+     let res = await fetch(global.API('https://hadi-api.herokuapp.com', '/api/detik'))
+     let json = await res.json()
+     let { title, img_url, time, link_url } = json.result[0]
+let kompis = `📺 *Detik News*
+📢 *Berita:* ${title}
+📁 *Terbit:* ${time}
+🛰 *Source Url:* ${link_url}`
+   conn.sendFile(m.chat, img_url, '', kompis, m)
+}
+
+handler.help = ['detik']
+handler.tags = ['berita']
+handler.command = /^detik(news)?$/i
+
+export default handler
