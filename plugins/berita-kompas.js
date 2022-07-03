602b65e9e1cf0cdee777bfeabada4b97e15af17e
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 15:37:37 +0700

    Update berita-kompas.js

--- plugins/berita-kompas.js
+++ plugins/berita-kompas.js
@@ -4,14 +4,15 @@ import fetch from 'node-fetch'
 let handler = async (m, {command, conn}) => {
 
      let src = await (await fetch('https://hadi-api.herokuapp.com/api/kompas')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-//     let json = await res.json()
+let json = await src.json()
+//     let json = src[Math.floor(Math.random() * src.length)]
+//     
 let { title, img, time, url } = json.result[0]
 let kompis = `📺 *Kompas News*
 📢 *Berita:* ${title}
 📁 *Type News:* ${time}
 🛰 *Source Url:* ${url}`
-        conn.sendButton(m.chat, `_${command}_`.trim(), kompis, img, [['😋 Lanjut Baca 🤗', `/${command}`]], m)
+   conn.sendFile(m.chat, img, '', kompis, m)
 }
 handler.help = ['kompasnews']
 handler.tags = ['berita']
