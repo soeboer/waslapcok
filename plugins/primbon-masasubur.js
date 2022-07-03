edea2812bf672df1cf26e850771ebad571b84074
Burhan Syam <burhansyam85@gmail.com>
2022-07-04 00:10:52 +0700

    Create primbon-masasubur.js

---
+++ plugins/primbon-masasubur.js
@@ -0,0 +1,18 @@
+import { Primbon } from 'scrape-primbon'
+
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+                if (!text) throw `Example : ${prefix + command} 12, 1, 2022, 28\n\nNote : ${prefix + command} hari pertama menstruasi, siklus`
+                let [tgl, bln, thn, siklus] = text.split`,`
+                
+                const primbon = new Primbon()           
+                let anu = await primbon.masa_subur(tgl, bln, thn, siklus)
+                if (anu.status == false) return m.reply(anu.message)
+                conn.sendText(m.chat, `⭔ *Hasil :* ${anu.message.result}\n⭔ *Catatan :* ${anu.message.catatan}`, m)
+}
+
+handler.help = ['subur'].map(v => v + ' <12, 1, 2022, 28>')
+handler.tags = ['primbon']
+handler.command = /^(subur)$/i
+
+export default handler
