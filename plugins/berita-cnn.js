672621e250427e2001df58ef833e862375fa71f1
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 16:24:24 +0700

    Update and rename berita-liputan6.js to berita-cnn.js

---
+++ plugins/berita-cnn.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+
+
+let handler = async (m, {command, conn}) => {
+//      let res = await (await fetch('https://hadi-api.herokuapp.com/api/kompas')).json()
+     let res = await fetch(global.API('https://hadi-api.herokuapp.com', '/api/berita'))
+     let json = await res.json()
+     let { judul, articleUrl, date_created, image, desc, body } = json.result[0]
+let kompis = `📺 *CNN News*
+📢 *Berita:* ${judul}
+📁 *Terbit:* ${date_created}
+🎯 *Tajuk :* ${desc}
+📑 *Berita :* ${body}
+🌎 *Link:* ${articleUrl}`
+   conn.sendFile(m.chat, image, '', kompis, m)
+}
+
+handler.help = ['cnn']
+handler.tags = ['berita']
+handler.command = /^cnn(news)?$/i
+export default handler
