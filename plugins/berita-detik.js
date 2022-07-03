5691263ba0bde410041fb0caa4d9f9db587703bd
Burhan Syam <burhansyam85@gmail.com>
2022-07-03 16:31:49 +0700

    Update berita-detik.js

--- plugins/berita-detik.js
+++ plugins/berita-detik.js
@@ -5,10 +5,10 @@ let handler = async (m, {command, conn}) => {
 //      let res = await (await fetch('https://hadi-api.herokuapp.com/api/kompas')).json()
      let res = await fetch(global.API('https://hadi-api.herokuapp.com', '/api/detik'))
      let json = await res.json()
-     let { title, img_url, time, link_url } = json.result[0]
+     let { title, img_url, Time, link_url } = json.result[0]
 let kompis = `📺 *Detik News*
 📢 *Berita:* ${title}
-📁 *Terbit:* ${time}
+📁 *Terbit:* ${Time}
 🛰 *Source Url:* ${link_url}`
    conn.sendFile(m.chat, img_url, '', kompis, m)
 }
