94ef842ec01ac18464072d5e90781176a590f15d
Burhan Syam <burhansyam85@gmail.com>
2022-07-04 02:13:10 +0700

    Update islami-iqro.js

--- plugins/islami-iqro.js
+++ plugins/islami-iqro.js
@@ -1,11 +1,9 @@
-import fetch from 'node-fetch'
-
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   
 let oh = `Contoh : ${usedPrefix + command} 3\n\nIQRA Yang tersedia : 1,2,3,4,5,6`
 		if (!text) throw oh
-		let yy = await fetch(`https://islamic-api-indonesia.herokuapp.com/api/data/pdf/iqra${text}`)
-		conn.sendFile(m.chat, {document: yy, mimetype: 'application/pdf', fileName: `iqro${text}.pdf`}, {quoted:m}).catch ((err) => m.reply(oh))
+		let yy = await conn.getFile(`https://islamic-api-indonesia.herokuapp.com/api/data/pdf/iqra${text}`)
+		conn.sendFile(m.chat, {document: yy.data, mimetype: 'application/pdf', fileName: `iqro${text}.pdf`}, {quoted:m}).catch ((err) => m.reply(oh))
 		}
 handler.help = ['iqro']
 handler.tags = ['islami']
