243cd285b302d732c764f49b620f8c6d9bd721af
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 04:52:41 +0700

    Update and rename nulis-nulis2.js to maker-nulis2.js

--- plugins/maker-nulis2.js
+++ plugins/maker-nulis2.js
@@ -67,7 +67,7 @@ let handler = async (m, { conn, args }) => {
         .stdout.on('data', chunk => bufs.push(chunk))
 }
 handler.help = ['n'].map(v => v + 'ulis2 <teks>')
-handler.tags = ['nulis']
+handler.tags = ['maker']
 handler.command = /^nulis2$/i
 
 export default handler
