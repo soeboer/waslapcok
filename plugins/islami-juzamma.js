1c6cea4f8d655544c32851d61633553b186b2ae2
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 03:03:21 +0700

    Update islami-juzamma.js

--- plugins/islami-juzamma.js
+++ plugins/islami-juzamma.js
@@ -1,9 +1,10 @@
 let handler = async (m, { args, usedPrefix, command }) => {
-	
+
 let er = `Juz Amma yang tersedia berformat : pdf, docx, pptx, xlsx
 Contoh: ${usedPrefix + command} pdf 
 `.trim()
     if (!args[0]) throw er
+	
     switch (args[0].toLowerCase()) {
         case 'pdf':
         case 'docx':
@@ -12,7 +13,7 @@ Contoh: ${usedPrefix + command} pdf
             let text = args.slice(1).join(' ')
             let pilih = args[0].toLowerCase()
 		await m.reply('baik,ditunggu dulu kak...')	
-		let hasil = await conn.getFile(`https://fatiharridho.my.id/database/islam/juz-amma-arab-latin-indonesia.${pilih}`)
+		let hasil = await conn.getFile(`https://github.com/burhansyam/media/blob/main/juz-amma-arab-latin-indonesia.${pilih}`)
 		let caption = `*Juz-Amma*\n\nFormat : ${pilih}`
                 conn.sendFile(m.chat, hasil.data, `Juz-Amma.${pilih}`, caption, m)
 	break
