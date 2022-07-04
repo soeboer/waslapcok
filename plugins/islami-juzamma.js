aed7553c334145a69927eb24721e4ed1bb9b3756
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 02:38:08 +0700

    Create islami-juzamma.js

---
+++ plugins/islami-juzamma.js
@@ -0,0 +1,27 @@
+let handler = async (m, { args, usedPrefix, command }) => {
+	
+let er = `Juz Amma yang tersedia berformat : pdf, docx, pptx, xlsx
+Contoh: ${usedPrefix + command} pdf 
+`.trim()
+    if (!args[0]) throw er
+    switch (args[0].toLowerCase()) {
+        case 'pdf':
+        case 'docx':
+        case 'pptx':
+        case 'xlsx':       
+            let text = args.slice(1).join(' ')
+            let pilih = args[0].toLowerCase()
+		await m.reply('baik,ditunggu dulu kak...')	
+		let hasil = await conn.getFile(`https://fatiharridho.my.id/database/islam/juz-amma-arab-latin-indonesia.${pilih}`)
+		let caption = `*Juz-Amma*\n\nFormat : ${pilih}`
+                conn.sendFile(m.chat, hasil.data, `Juz-Amma.${pilih}`, caption, m)
+	break
+        default:
+            throw er
+    }
+		}
+handler.help = ['juzamma']
+handler.tags = ['islami']
+handler.command = /^(juzamma)$/i
+
+export default handler
