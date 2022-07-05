7eb14d4b5b8b7d11fa4d15452867594f1c30ac2f
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 13:42:21 +0700

    Create fun-senja.js

---
+++ plugins/fun-senja.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = await axios("https://api.burhansyam.com/bot/senja.json")
+let json = res.data
+let senja = json.senja
+let author = json.author
+conn.sendButton(m.chat, "_*Kata Anak Senja :*_", senja, author, [['😢 Acak Quote Senja 🥲', `${usedPrefix + command}`]], m)}
+handler.help = ['senja']
+handler.tags = ['fun']
+handler.command = /^(senja)$/i
+export default handler
