ec9b7355d14a341c2a498c53ac7bcbd6de743e9d
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 18:35:39 +0700

    Update a-kodepos.js

--- plugins/a-kodepos.js
+++ plugins/a-kodepos.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `*Perintah ini untuk mencari kode pos berdasarkan kota/pencarian*\n\ncontoh:\n${usedPrefix + command} Ponjong`
     
-    let res = await fetch(global.API('https://kodepos.vercel.app', '/search', { q: text }, 'APIKEY'))
+    let res = await fetch(global.API('https://kodepos.vercel.app', '/search', { q: text }))
     if (res.status != 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
     let mes = json.data.map((v, i) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
-//     m.reply(mes)
-    	          conn.reply(m.chat, mes, m)
+    m.reply(mes)
+//     	          conn.reply(m.chat, mes, m)
 
 }
 
