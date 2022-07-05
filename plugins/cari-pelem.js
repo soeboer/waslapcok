a7877daa7d51b623a3d761f423dd94019ba50114
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 15:52:41 +0700

    Update cari-pelem.js

--- plugins/cari-pelem.js
+++ plugins/cari-pelem.js
@@ -1,23 +1,21 @@
-import { xa } from 'xfarr-api'
+import { film } from 'xfarr-api'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
-
-let handler = async (m, { usedPrefix, command, conn:fur, args }) => {
-  
-if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} spiderman`
-
-const data = await xa.search.film(args[0]).then(async data => {
+let handler = async (m, { conn, text }) => {
+    if (!text) throw `*[❗INFO❗] Masukan Judul Film Yang Ingin Kamu Cari*`
+    let data  = await film(text)
+    let datathumb = await(await fetch(data[0].thumb)).buffer()
+// let gbr = data[0].thumb  
 let txt = `*--------「 FILM-SEARCH 」--------*\n\n`
 for (let i of data) {
 txt += `*📫 Judul :* ${i.judul}\n`
-txt += `*🎞️  Tipe  :* ${i.type}\n`
+txt += `*🎞️ Tipe  :* ${i.type}\n`
 txt += `*📟 Kualitas :* ${i.quality}\n`
-txt += `*📮Upload :* ${i.upload}\n`
-txt += `*🔗 Url :* ${await shortlink(i.link)}\n-----------------------------------------------------\n`
+txt += `*📮 Upload :* ${i.upload}\n`
+txt += `*🔗 Url :* ${i.link}\n------------------------------------\n`
 }
-
-conn.sendButton(m.chat, await (await fetch(data[0].thumb)).buffer(), txt, wm, 'pencet', 'ok', m)
+conn.sendFile(m.chat, datathumb, '', txt, m)
 })
 }
 handler.help = ['film <keyword>']
@@ -25,7 +23,3 @@ handler.tags = ['pencarian', 'film']
 handler.command = /^(film)$/i
 
 export default handler
-
-async function shortlink(url){
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''}
