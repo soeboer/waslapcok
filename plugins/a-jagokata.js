5ced19fe5d507378facfe40d9f11d22035542661
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 14:04:02 +0700

    Update a-jagokata.js

--- plugins/a-jagokata.js
+++ plugins/a-jagokata.js
@@ -1,12 +1,29 @@
-import { quotes } from './lib/jagokata.js'
+import axios from 'axios'
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `Contoh : ${usedPrefix + command} makan`
-                const quotes = new quotes()   
-                let anu = await quotes.data(text)
-                if (anu.status == false) return m.reply(anu.message)
-                conn.reply(m.chat, `⭔ *Quotes :* ${anu.message.quote}\n⭔ *Penulis :* ${anu.message.author}\n⭔ *Biografi :* ${anu.message.bio}`, m)
+// let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+// if (!text) throw `Contoh : ${usedPrefix + command} makan`
+
+//                 let res = await axios("https://violetics.pw/api/information/jagokata?apikey=beta&nama=${text}")
+//                 let json = res.data
+//                 let senja = json.senja
+               
+
+//                 if (anu.status == false) return m.reply(anu.message)
+//                 conn.reply(m.chat, `⭔ *Quotes :* ${anu.message.quote}\n⭔ *Penulis :* ${anu.message.author}\n⭔ *Biografi :* ${anu.message.bio}`, m)
+// }
+
+let handler = async(m, { conn, text, usedPrefix }) => {
+
+    if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'rindu', m)
+    axios.get(`https://violetics.pw/api/information/jagokata?apikey=beta&nama=` + text)
+        .then((res) => {
+          let hasil = `*⭔ Quotes Kata ${text} :*\n*${res.data.results.quote}*`
+//           `⭔ *Quotes :* ${res.data.quote}\n⭔ *Penulis :* ${anu.message.author}\n⭔ *Biografi :* ${anu.message.bio}`
+            conn.reply(m.chat, hasil, m)
+        })
+        .catch(_ => m.reply('Quote Tidak Ditemukan!'))
 }
+
 
 
 handler.help = ['jagokata'].map(v => v + ' [name]')
