eae7559d5c142fe6298e5fe0b3970c2cd2872419
Burhan Syam <burhansyam85@gmail.com>
2022-07-06 03:32:30 +0700

    Update a-cekresi.js

--- plugins/a-cekresi.js
+++ plugins/a-cekresi.js
@@ -11,11 +11,12 @@ sicepat
     if (!teks) return conn.reply(m.chat, 'nomor resinya?', m)
 
   await m.reply('Sabar Kak saya cek dulu...')
- let hasil = await fetch('https://api.burhansyam.com/bot/resi/?kurir=' + kurir + '&resi=' + teks + `&apikey=bajingan99`)
+ let fucek = await fetch('https://api.burhansyam.com/bot/resi/?kurir=' + kurir + '&resi=' + teks + `&apikey=bajingan99`)
 //  let hasil = await conn.getFile(`https://violetics.pw/api/photooxy/${effect}?text=${teks}&apikey=beta`)
 //  let caption = `*PHOTOOXY*\n\nEffect : ${effect}`
-  m.reply(`${hasil}`)
+//   m.reply(`${hasil}`)
 //     conn.sendFile(m.chat, hasil.data, 'photooxy.jpg', caption, m)
+    conn.reply(m.chat,`fucek`.trim(), m)
 }
 
 handler.help = ['cekresi <kurir noresi>']
