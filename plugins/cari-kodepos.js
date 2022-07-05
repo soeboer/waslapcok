7d24d4c8a21779e8962823fe08df41bdbaae5cb8
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 17:21:07 +0700

    Update cari-kodepos.js

--- plugins/cari-kodepos.js
+++ plugins/cari-kodepos.js
@@ -3,15 +3,12 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `*Perintah ini untuk mencari kode pos berdasarkan kota/pencarian*\n\ncontoh:\n${usedPrefix + command} Ponjong`
     let res = await fetch(global.API('https://kodepos.vercel.app', '/search', { q: text }))
-    
-         let json = await res.json()
-//     if (res.status != 200) throw await res.text()
-//     let json = await res.json()
-//     if (!json.status) throw json
+    if (res.status != 200) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
          
-         let { province, city, subdistrict, urban, postalcode } = json.data
-         let kopos = json.data.map((v) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join`\n`                                  
-                                  
+//          let { province, city, subdistrict, urban, postalcode } = json.data
+         let kopos = json.data.map((v) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join`\n`                                                 
 //`${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
   
     m.reply(kopos)
