2facabeb4c547c6c7df332d4982d14d94b290009
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 18:24:26 +0700

    Update and rename cari-kodepos.js to a-kodepos.js

---
+++ plugins/a-kodepos.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `*Perintah ini untuk mencari kode pos berdasarkan kota/pencarian*\n\ncontoh:\n${usedPrefix + command} Ponjong`
+    
+    let res = await fetch(global.API('kodepos', '/search', { q: text }, 'APIKEY'))
+    if (res.status != 200) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
+    let mes = json.data.map((v, i) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
+    m.reply(`${mes}`)
+
+}
+
+handler.help = ['kodepos].map(v => v + ' <query>'']
+handler.tags = ['pencarian']
+handler.command = /^kodepos$/i
+export default handler
