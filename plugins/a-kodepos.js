fba3a795726c29d9825e2f2e8a852dd559b48667
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 18:43:47 +0700

    Update a-kodepos.js

--- plugins/a-kodepos.js
+++ plugins/a-kodepos.js
@@ -3,17 +3,17 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `*Perintah ini untuk mencari kode pos berdasarkan kota/pencarian*\n\ncontoh:\n${usedPrefix + command} Ponjong`
     
-    let res = await fetch(global.API('https://kodepos.vercel.app', '/search', { q: text }))
+    let res = await fetch(global.API('https://api.burhansyam.com', '/bot/kodepos/', { q: text }))
     if (res.status != 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
-    let mes = json.data.map((v, i) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
+    let mes = json.result.map((v, i) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
     m.reply(mes)
 //     	          conn.reply(m.chat, mes, m)
 
 }
 
-handler.help = ['kodepos].map(v => v + ' <Ponjong>'']
+handler.help = ['kodepos <kota>']
 handler.tags = ['pencarian']
 handler.command = /^kodepos$/i
 export default handler
