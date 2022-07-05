c5dcbf0ae2280cc9c99ddb16c83157c8f16c998c
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 18:10:17 +0700

    Update cari-kodepos.js

--- plugins/cari-kodepos.js
+++ plugins/cari-kodepos.js
@@ -1,17 +1,15 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `*Perintah ini untuk mencari kode pos berdasarkan kota/pencarian*\n\ncontoh:\n${usedPrefix + command} Ponjong`
-    let res = await fetch(global.API('https://kodepos.vercel.app', '/search', { q: text }))
-    if (res.status != 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
-         
-//          let { province, city, subdistrict, urban, postalcode } = json.data
-         let kopos = json.data.map((v) => `Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join`\n`                                                 
-//`${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
-  
-m.reply(`${kopos}`)
+let handler = async(m, { conn, text, usedPrefix }) => {
+
+    if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'Ponjong', m)
+    axios.get(`https://kodepos.vercel.app/search/?q=` + text)
+        .then((res) => {
+          let hasil = res.data.map((v) => `Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join`\n`  
+          
+          conn.reply(m.chat, hasil, m)
+        })
+        .catch(_ => m.reply('Kodepos Tidak Ditemukan!'))
 }
 
 handler.help = ['kodepos].map(v => v + ' <query>'']
