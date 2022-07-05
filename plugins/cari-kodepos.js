a0b059a2cbca4049c7e5c4e52d39748ad5c2ecf9
Burhan Syam <burhansyam85@gmail.com>
2022-07-05 17:17:48 +0700

    Update cari-kodepos.js

--- plugins/cari-kodepos.js
+++ plugins/cari-kodepos.js
@@ -8,11 +8,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 //     if (res.status != 200) throw await res.text()
 //     let json = await res.json()
 //     if (!json.status) throw json
-    let mes = json.data((v, i) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
-    m.reply(mes)
+         
+         let { province, city, subdistrict, urban, postalcode } = json.data
+         let kopos = json.data.map((v) => `${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join`\n`                                  
+                                  
+//`${i + 1}. Provinsi: ${v.province}\nKota: ${v.city}\nKecamatan: ${v.subdistrict}\nPerkotaan: ${v.urban}\nKode Pos: ${v.postalcode}`).join('\n\n')
+  
+    m.reply(kopos)
 }
 
-handler.help = ['kodepos <kota>']
+handler.help = ['kodepos].map(v => v + ' <query>'']
 handler.tags = ['pencarian']
 handler.command = /^kodepos$/i
 export default handler
