5281f408dcf83b86e374d96bb557754ac5bdb1a9
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 02:59:43 +0700

    Update info-gsmarena.js

--- plugins/info-gsmarena.js
+++ plugins/info-gsmarena.js
@@ -1,9 +1,13 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => { 
+	
 if (!text) throw 'Brand atau Type Apa?\n *Contoh :* ${usedPrefix + command} redmi note 10s'
-        let anu = await fetch(`https://yx-api.herokuapp.com/api/search/gsmarena?query=${text}`)				
-				let kaslak = `* GSM ARENA *\n\n
+	
+//         let anu = await fetch(`https://yx-api.herokuapp.com/api/search/gsmarena?query=${text}`)				
+let anu = await fetch(global.API('https://yx-api.herokuapp.com', '/api/search/gsmarena', { query: text }))
+	
+let kaslak = `* GSM ARENA *\n\n
 *Name* : ${anu.judul}
 *Rilis* : ${anu.rilis}
 *Ukuran* : ${anu.ukuran}
@@ -18,12 +22,16 @@ if (!text) throw 'Brand atau Type Apa?\n *Contoh :* ${usedPrefix + command} redm
 *Batrai* : ${anu.batrai}
 *Merek Batrai* : ${anu.merek_batre}
 *Detail* : ${anu.detail}`			
-//var kontoll = await getBuffer(anu.result.owner.profile_pic)	
-				//sendButLocation(from, `${reply22}`,`© drips`, {jpegThumbnail:kontoll}, [{buttonId:`${prefix}menu`,buttonText:{displayText:'OKE'},type:1}], {contextInfo: { mentionedJid: [sender, owner]}})
-let gimbir = `$(anu.thumb)`
+// let gimbir = `$(anu.thumb)`
 
-        conn.sendMessage(m.chat, { image: gimbir, caption: `${kaslak}` }, { quoted: m}).catch((err) => m.reply('*Spesifikasi HP tidak ditemukan*'))
-	            }
+//         conn.sendMessage(m.chat, { image: gimbir, caption: `${kaslak}` }, { quoted: m}).catch((err) => m.reply('*Spesifikasi HP tidak ditemukan*'))
+// 	            }
+
+conn.reply(m.chat, kaslak, m)
+        })
+        .catch(_ => m.reply('*Spesifikasi HP tidak ditemukan*'))
+}
+
 
 handler.help = ['garena <type HP>']
 handler.tags = ['info']
