31e6266bae90cff7d4a67869ae70c9f85795c4d7
Burhan Syam <burhansyam85@gmail.com>
2022-07-06 14:19:50 +0700

    Update tools-wattpad.js

--- plugins/tools-wattpad.js
+++ plugins/tools-wattpad.js
@@ -3,13 +3,15 @@ import { WattPads } from '@dhnapi/wattpad.js'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
                 if (!text) throw `Contoh : ${usedPrefix + command} love`
                                 let wattpad = new WattPads(); 
-                let cari = wattpad.set('query', 'text');
-                let anu = await wattpad.search(cari);
+
+async function wattpad.search(text) {  
+                 wattpad.set('query', 'text');
+               let anu = await wattpad.search();
                 if (anu.status == false) return m.reply(anu.response)
 //                 conn.reply(m.chat, `⭔ *Judul :* ${anu.response}\n⭔ *Info :* ${anu.response}\n⭔ *Deskripsi :* ${anu.response}`, m)
                   conn.reply(m.chat, `⭔ *Judul :* ${anu.response}`, m)
 }
-handler.help = ['wattpadd'].map(v => v + ' [nama]')
+handler.help = ['wattpadd'].map(v => v + ' [tentang]')
 handler.tags = ['tools']
 handler.command = ['wattpadd']
 
