bb4e599377f1d34a04e804adba4d3b8d6f5a3523
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 03:25:10 +0700

    Update info-gsmarena.js

--- plugins/info-gsmarena.js
+++ plugins/info-gsmarena.js
@@ -6,8 +6,8 @@ let handler = async(m, { conn, text, usedPrefix }) => {
     axios.get(`https://yx-api.herokuapp.com/api/search/gsmarena?query=` + text)
         .then((res) => {
 	    
-let hasil = `* GSM ARENA *\n*Name* : ${res.judul}\n*Rilis* : ${res.rilis}\n*Ukuran* : ${res.ukuran}\n*Model* : ${res.type}\n*Storage* : ${res.storage}\n*Display* : ${res.display}\n*Inchi* : ${res.inchi}\n*Pixel* : ${res.pixel}\n*VideoPixel* : ${res.videoPixel}\n*Ram* : ${res.ram}\n*Chipset* : ${res.chipset}\n*Batrai* : ${res.batrai}\n*Merek Batrai* : ${res.merek_batre}\n*Detail* : ${res.detail}`
-
+// let hasil = `* GSM ARENA *\n*Name* : ${res.judul}\n*Rilis* : ${res.rilis}\n*Ukuran* : ${res.ukuran}\n*Model* : ${res.type}\n*Storage* : ${res.storage}\n*Display* : ${res.display}\n*Inchi* : ${res.inchi}\n*Pixel* : ${res.pixel}\n*VideoPixel* : ${res.videoPixel}\n*Ram* : ${res.ram}\n*Chipset* : ${res.chipset}\n*Batrai* : ${res.batrai}\n*Merek Batrai* : ${res.merek_batre}\n*Detail* : ${res.detail}`
+let hasil = `Type HP ${text}\n*${res.judul} `
             conn.reply(m.chat, hasil, m)
         })
         .catch(_ => m.reply('Spesifikasi HP Tidak Ditemukan!'))
