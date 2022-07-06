8b700403585d57afccf655f1a76b58b40ecfcbd6
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 03:28:03 +0700

    Update info-gsmarena.js

--- plugins/info-gsmarena.js
+++ plugins/info-gsmarena.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix }) => {
         .then((res) => {
 	    
 // let hasil = `* GSM ARENA *\n*Name* : ${res.judul}\n*Rilis* : ${res.rilis}\n*Ukuran* : ${res.ukuran}\n*Model* : ${res.type}\n*Storage* : ${res.storage}\n*Display* : ${res.display}\n*Inchi* : ${res.inchi}\n*Pixel* : ${res.pixel}\n*VideoPixel* : ${res.videoPixel}\n*Ram* : ${res.ram}\n*Chipset* : ${res.chipset}\n*Batrai* : ${res.batrai}\n*Merek Batrai* : ${res.merek_batre}\n*Detail* : ${res.detail}`
-let hasil = `Type HP ${text}\n*${res.judul} `
+let hasil = `Type HP ${text}\n*${res.data.judul} `
             conn.reply(m.chat, hasil, m)
         })
         .catch(_ => m.reply('Spesifikasi HP Tidak Ditemukan!'))
