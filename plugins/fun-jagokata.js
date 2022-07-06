a6c83c36630ac0bdf7b56bda7f10fb799ab77bb7
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 03:19:31 +0700

    Update fun-jagokata.js

--- plugins/fun-jagokata.js
+++ plugins/fun-jagokata.js
@@ -1,16 +1,5 @@
 import axios from 'axios'
 
-// let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-// if (!text) throw `Contoh : ${usedPrefix + command} makan`
-
-//                 let res = await axios("https://violetics.pw/api/information/jagokata?apikey=beta&nama=${text}")
-//                 let json = res.data
-//                 let senja = json.senja
-               
-
-//                 if (anu.status == false) return m.reply(anu.message)
-//                 conn.reply(m.chat, `⭔ *Quotes :* ${anu.message.quote}\n⭔ *Penulis :* ${anu.message.author}\n⭔ *Biografi :* ${anu.message.bio}`, m)
-// }
 
 let handler = async(m, { conn, text, usedPrefix }) => {
 
@@ -21,7 +10,6 @@ let handler = async(m, { conn, text, usedPrefix }) => {
           let bio = `${res.data.result.bio}`
           let au = `${res.data.result.author}`
           let hasil = `*⭔ Quotes Kata ${text} :*\n*${res.data.results.quote}\n⭔ *Penulis :* ${au}\n⭔ *Biografi :* ${bio} *`
-//           `⭔ *Quotes :* ${res.data.quote}\n⭔ *Penulis :* ${anu.message.author}\n⭔ *Biografi :* ${anu.message.bio}`
             conn.reply(m.chat, hasil, m)
         })
         .catch(_ => m.reply('Quote Tidak Ditemukan!'))
