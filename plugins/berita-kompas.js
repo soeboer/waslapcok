bc642c118758e64810a95f417e5314d412b5d332
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 00:44:33 +0700

    Update berita-kompas.js

--- plugins/berita-kompas.js
+++ plugins/berita-kompas.js
@@ -2,17 +2,19 @@ import fetch from 'node-fetch'
 
 
 let handler = async (m, {command, conn}) => {
-//      let res = await (await fetch('https://hadi-api.herokuapp.com/api/kompas')).json()
      let res = await fetch(global.API('https://hadi-api.herokuapp.com', '/api/kompas'))
      let json = await res.json()
-     let { title, img, time, url } = json.result[0]
-let kompis = `📺 *Kompas News*
-📢 *Berita:* ${title}
-📁 *Type News:* ${time}
-🛰 *Source Url:* ${url}`
-   conn.sendFile(m.chat, img, '', kompis, m)
+     let kompos = json.result.map((v, i) => `${i + 1}.📺 Judul : ${v.title}\n📢 Terbit : ${v.time}\n🔑 Link : ${v.url}`).join('\n\n')
+     m.reply(kompos)
+     
+// let { title, img, time, url } = json.result[0]
+// let kompis = `📺 *Kompas News*
+// 📢 *Berita:* ${title}
+// 📁 *Type News:* ${time}
+// 🛰 *Source Url:* ${url}`
+//    conn.sendFile(m.chat, img, '', kompis, m)
 }
-handler.help = ['kompasnews']
+handler.help = ['kompas']
 handler.tags = ['berita']
 handler.command = /^kompas(news)?$/i
 
