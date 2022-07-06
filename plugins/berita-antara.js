8b0d434596162e95e3ba5e84bbf8bb99ab2486d8
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 01:36:36 +0700

    Create berita-antara.js

---
+++ plugins/berita-antara.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+import { Antaranews } from '@bochilteam/scraper'
+
+let handler = async(m, { conn }) => {
+   var a = await antaranews()
+   var b = JSON.parse(JSON.stringify(a))
+//    var c = await conn.rand(b)
+   var c = b[Math.floor(Math.random() * b.length)]
+   var { title, link, image, date } = c
+   var sell = `📺 *Antara News*
+🌐 *Berita:* ${title}
+⌚ *Date:* ${date}
+🛰️ *Source Url:* ${link}`.trim()
+   conn.sendButton(m.chat, `${sell}`, wm, null, [['AntaraNews', '/liputan6']], m)
+
+}
+handler.help = ['antara']
+handler.tags = ['berita']
+handler.command = /^antara(news)?$/i
+export default handler
