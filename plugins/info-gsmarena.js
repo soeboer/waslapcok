ee8221baecb568a974a53b68cf13317767cea6be
Burhan Syam <burhansyam85@gmail.com>
2022-07-07 13:36:06 +0700

    Update info-gsmarena.js

--- plugins/info-gsmarena.js
+++ plugins/info-gsmarena.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, text, usedPrefix }) => {
     axios.get(`https://yx-api.herokuapp.com/api/search/gsmarena?query=` + text)
         .then((res) => {
 	    
-let hasil = `*Rincian Type HP ${text}* \n *Name* : ${res.data.judul}\n*Rilis* : ${res.data.rilis}\n*Ukuran* : ${res.data.ukuran}\n*Model* : ${res.data.type}\n*Storage* : ${res.data.storage}\n*Display* : ${res.data.display}\n*Inchi* : ${res.data.inchi}\n*Pixel* : ${res.data.pixel}\n*VideoPixel* : ${res.data.videoPixel}\n*Ram* : ${res.data.ram}\n*Chipset* : ${res.data.chipset}\n*Batrai* : ${res.data.batrai}\n*Merek Batrai* : ${res.data.merek_batre}\n*Detail* : ${res.data.detail.replace('\n\n', '')}`
+let hasil = `*Rincian Type HP ${text}* \n *Name* : ${res.data.judul}\n*Rilis* : ${res.data.rilis}\n*Ukuran* : ${res.data.ukuran}\n*Model* : ${res.data.type}\n*Storage* : ${res.data.storage}\n*Display* : ${res.data.display}\n*Inchi* : ${res.data.inchi}\n*Pixel* : ${res.data.pixel}\n*VideoPixel* : ${res.data.videoPixel}\n*Ram* : ${res.data.ram}\n*Chipset* : ${res.data.chipset}\n*Batrai* : ${res.data.batrai}\n*Merek Batrai* : ${res.data.merek_batre}\n*Detail* : ${res.data.detail.replace('\n\n\t\n\n', '')}`
 
             conn.reply(m.chat, hasil, m)
         })
