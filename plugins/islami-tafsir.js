2b5cee8b00952165d593c669b6c63f6aa622bcb7
Burhan Syam <burhansyam85@gmail.com>
2022-07-09 02:35:01 +0700

    Update islami-tafsir.js

--- plugins/islami-tafsir.js
+++ plugins/islami-tafsir.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { command, conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Tafsir tentang apa?*`
 let json = await fetch(global.API('https://docs-jojo.herokuapp.com', '/api/tafsir', { q: text }))
 let tafsir = json.result.map((v, i) => `${i + 1}.📓 Surat : ${v.tafsir}\n📖 Deskripsi : ${v.deskripsi}`).join('\n\n')
