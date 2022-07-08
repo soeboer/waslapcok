224b3380072bcc36cffb661641ed10a497c30b69
Burhan Syam <burhansyam85@gmail.com>
2022-07-08 15:42:17 +0700

    Create info-cekab.js

---
+++ plugins/info-cekab.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, text }) => {
+let [nopol, teks] = text.split ` `
+
+    if (!nopol) return conn.reply(m.chat, 'Berapa Nopolnya ?', m)
+    if (!teks) return conn.reply(m.chat, 'Huruf Belakangnya ?', m)
+  await m.reply('Sabar bestie saya cek dulu...')
+ let res = await fetch(`https://api.burhansyam.com/bot/pajek/?no=${nopol}&dd=${teks}`)
+let json = await res.json()
+let { nopolisi, nmmerekkb, nmmodelkb, tahunkb, swdkllj, pkb, pkbswd, tgakhirpkb } = json.results
+
+let pelat = `
+✨ *Nopol   :* ${nopolisi}
+🎆 *Merk KB :* ${nmmerekkb}
+💬 *Model KB:* ${nmmodelkb}
+💌 *Tahun   :* ${tahunkb}
+❤️ *SWD KB  :* ${swdkllj}
+👥 *Pajak KB:* ${pkb}
+💚 *PKB SWD :* ${pkbswd}
+🌐 *AkrPjak :* ${tgakhirpkb}`      
+//  conn.reply(m.chat, `${res.result}`, m)
+           conn.reply(m.chat, pelat, m)
+}
+
+handler.help = ['cekab <1895 MD>']
+handler.tags = ['info']
+handler.command = /^(cekab)$/i
+
+export default handler
