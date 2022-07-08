d6b78e234505cfb789d6362c1c51bf9acbd25f80
Burhan Syam <burhansyam85@gmail.com>
2022-07-09 02:27:58 +0700

    Create islami-tafsir.js

---
+++ plugins/islami-tafsir.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+let handler = async (m, { conn, text }) => {
+if (!text) throw `*[❗INFO❗] Masukan Tafsir tentang apa?*`
+let json = await fetch(global.API('https://docs-jojo.herokuapp.com', '/api/tafsir', { q: text }))
+let tafsir = json.result.map((v, i) => `${i + 1}.📓 Surat : ${v.tafsir}\n📖 Deskripsi : ${v.deskripsi}`).join('\n\n')
+m.reply(tafsir)
+  }
+handler.help = ['tafsir <tentang>']
+handler.tags = ['islami']
+handler.command = /^(tafsir)$/i
+export default handler
