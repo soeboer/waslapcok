9c953f6a1db0c836779858e13690ccdac6d2e459
Burhan Syam <burhansyam85@gmail.com>
2022-07-10 02:09:04 +0700

    Update maker-penjara.js

--- plugins/maker-penjara.js
+++ plugins/maker-penjara.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, usedprefix }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendButton(m.chat, ' penjara', author, global.API('https://some-random-api.ml', '/canvas/jail', {
+    conn.sendFile(m.chat, ' penjara', author, global.API('https://some-random-api.ml', '/canvas/jail', {
         avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), [['penjara', `${usedprefix}penjara`]], m)
+    }), 'error.png', `*Tahanan Mertua*`,m)
 }
 
 handler.help = ['penjara']
