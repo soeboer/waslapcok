380e1fbfbe1bb5a4ad18de5a202d5881568e6b8e
Burhan Syam <burhansyam85@gmail.com>
2022-07-10 03:19:15 +0700

    Update maker-blur.js

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, usedprefix }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendButton(m.chat, ' blurry', author, global.API('https://some-random-api.ml', '/canvas/blur', {
+    conn.sendFile(m.chat, ' blurry', author, global.API('https://some-random-api.ml', '/canvas/blur', {
         avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), [['blur', `${usedprefix}blur`]], m)
+    }), 'error.png', `*Blur*`, m)
 }
 
 handler.help = ['blur']
