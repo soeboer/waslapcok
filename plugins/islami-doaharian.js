a227beec05f8ae5d1d5c09b423e78f8a2faf880f
Burhan Syam <burhansyam85@gmail.com>
2022-07-09 18:57:25 +0700

    Update islami-doaharian.js

--- plugins/islami-doaharian.js
+++ plugins/islami-doaharian.js
@@ -1,17 +1,15 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-  await m.reply(global.wait)
-  var a = await fetch(global.API('https://api.zacros.my.id','/islami/doaseharian'))
-//   let json = await res.json()
-//   if (res.status != 200) throw json
-//   if (json.result.error) throw json.result.message
-//   let { surah, arab, latin } = json.result
-   var b = JSON.parse(JSON.stringify(a))
-   var c = b[Math.floor(Math.random() * b.length)]
+  
+await m.reply('saya carikan dulu kak...')
+  let res = await fetch(global.API('https://api.zacros.my.id','/islami/doaseharian'))
+  var json = await res.json()
+//   let { image, hari_ini, description, angka_keberuntungan, peribahasa_cina, about } = json.result
+   var c = json.result[Math.floor(Math.random() * json.result.length)]
    var { surah, arab, latin } = c 
   
-  let caption = `
+  var caption = `
 *「 Doa Harian 」*
 ${surah}
 ${arab}
