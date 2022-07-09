334fd6057932182bf486a54f250e49ddbbd4727f
Burhan Syam <burhansyam85@gmail.com>
2022-07-10 04:24:09 +0700

    Update islami-jadwalsholat.js

--- plugins/islami-jadwalsholat.js
+++ plugins/islami-jadwalsholat.js
@@ -3,9 +3,8 @@ let handler = async (m, { text, usedPrefix, command }) => {
     if (!text) throw `Use example ${usedPrefix}${command} semarang`
     const res = await jadwalsholat(text)
     m.reply(`
-Jadwal Sholat *${text}*
-
-${Object.entries(res.today).map(([name, data]) => `*Sholat ${name}:* ${data}`).join('\n').trim()}
+🕌 Jadwal Sholat *${text}* 🕋
+${Object.entries(res.today).map(([name, data]) => ` ⏰*Sholat ${name}:* ${data}`).join('\n').trim()}
 `.trim())
 }
 handler.help = ['sholat <daerah>']
