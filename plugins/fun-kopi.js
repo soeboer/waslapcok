587fde203baff043fe0278289e0ad38136fa109d
Burhan Syam <burhansyam85@gmail.com>
2022-07-10 04:51:57 +0700

    Update fun-kopi.js

--- plugins/fun-kopi.js
+++ plugins/fun-kopi.js
@@ -1,12 +1,13 @@
-import fetch from "node-fetch"
+import fetch from 'node-fetch'
 
 let handler = async (m, {command, conn}) => {
 await m.reply('saya buatkan dulu maszeeh...')
   let res = await fetch(global.API("https://coffee.alexflipnote.dev", "/random.json")
   let json = await res.json();
- let { kipo } = json.file 
-        conn.sendButton(m.chat, `_${command}_`.trim(), `${kipo}`, kipo, [['😋 Ngopi Lagi 🤗', `/${command}`]], m)
- 
+ let { kipo } = json.file
+ let img = await conn.getFile(`${kipo}`)
+ var capt = `🐦 Selamat Menikmati 🐦`
+        conn.sendButton(m.chat, `_${command}_`.trim(), capt, img.data, [['😋 Ngopi lagi 🤗', `/${command}`]], m)
 };
 handler.help = ["kopi"];
 handler.tags = ["fun"];
