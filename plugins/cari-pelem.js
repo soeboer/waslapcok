25495780903ea8b88266affb18f582b03db9712d
Burhan Syam <burhansyam85@gmail.com>
2022-07-11 01:09:41 +0700

    Update cari-pelem.js

--- plugins/cari-pelem.js
+++ plugins/cari-pelem.js
@@ -2,21 +2,21 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Judul Film Yang Ingin Kamu Cari*`
+await m.reply('saya carikan dulu bestie...')
 let res = await fetch(global.API('https://api.burhansyam.com', '/bot/pelem/', { q: text }))
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { title, thumb, views, genre, quality, country, duration, release, result } = json.result
 let dondot = json.result.result.map((v, i) => `${i + 1}.📺 Server : ${v.name}\n🔑 Link : ${v.url}`).join('\n\n')
-let animeingfo = `
-✨ *Title:* ${title}
+let peleme = `
+✨ *Judul:* ${title}
 🌐 *Tahun Rilis*: ${release}
 💚 *Durasi:* ${duration}
 ❤️ *Kualitas:* ${quality}
-💌 *Rating:* ${genre}
+👥 *Rating & Sutradara:* ${genre}
 💬 *Genre:* ${views}
-👥 *Negara:* ${country}
 💋 *Link Download*:\n ${dondot}`
-conn.sendFile(m.chat, thumb, '', animeingfo, m)
+conn.sendFile(m.chat, thumb, '', peleme, m)
 }
 handler.help = ['pelem <judul>']
 handler.tags = ['pencarian']
