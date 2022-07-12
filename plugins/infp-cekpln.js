ade9ec22b01c0d577a5fc933b44cac9ab97db0fa
Burhan Syam <burhansyam85@gmail.com>
2022-07-13 02:59:51 +0700

    Create infp-cekpln.js

---
+++ plugins/infp-cekpln.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, text }) => {
+// let [teks, nopol, akhir] = text.split ` `
+    if (!text) return conn.reply(m.chat, 'ID Pelanggan ? \n contoh : .pln 521050558087)
+  await m.reply('Sabar bestie saya cek dulu...')
+let res = await fetch(`https://api.burhansyam.com/bot/pln/?id=${text}`)
+let json = await res.json()
+let { print, bill_number, bill_period } = json
+let pelan = `🚧 *Tagihan PLN Periode : ${bill_period}
+${print}`      
+           conn.reply(m.chat, pelan, m)
+}
+
+handler.help = ['pln <ID Pelanggan>']
+handler.tags = ['info']
+handler.command = /^(pln)$/i
+
+export default handler
