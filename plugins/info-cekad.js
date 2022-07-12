832ba7cf816bbce9bcd1d372c14dccb14deb8e95
Burhan Syam <burhansyam85@gmail.com>
2022-07-12 14:43:06 +0700

    Update info-cekad.js

--- plugins/info-cekad.js
+++ plugins/info-cekad.js
@@ -7,7 +7,7 @@ let [teks, nopol, akhir] = text.split ` `
     if (!nopol) return conn.reply(m.chat, '[❗INFO❗] Berapa Nopolnya ?\n Contoh : .cekad AD 1061 MP', m)
     if (!akhir) return conn.reply(m.chat, 'Huruf Belakangnya ? \n contoh : MJ, KL, FD', m)  
   await m.reply('Sabar bestie saya cek dulu...')
- let res = await fetch(`https://api.burhansyam.com/bot/sakpole/?na=${teks}&no=${nopol}&nc=${akhir}`)
+let res = await fetch(`https://api.burhansyam.com/bot/sakpole/?na=${teks}&no=${nopol}&nc=${akhir}`)
 let json = await res.json()
 let { kd_wilayah, kode_angka, kd_seri, merek, tipe, model, bbm, warna_tnkb, milikke, thn_buat, cylinder, jml_sumbu, sts_blokir, tgl_stnk, info_nik, thn, rincian, bulan, hari, bbn2_pokok, thn2, pnbp, kp, lama_tunggakan, tgl_jatuh_tempo, total_pkb_pokok, total_pkb_denda, jumlah_pkb, total_jr_pokok, total_jr_denda, jumlah_jr, total, lokasi_samsat, tgl_blokir, ket_blokir, tgl_pengenaan } = json
 // let rincian = json.result.rincian.map((v, i) => `${i + 1}. Masa Akhir Berlaku: ${v.masa_akhir_berlaku_pajak}\nLama Tunggakan: ${v.lama_tunggakan}\nPlus Denda: ${v.total}\nKode Terlambat: ${v.terlambat}`).join('\n\n')
@@ -24,6 +24,7 @@ let pelat = `
 💵 *Pajak KB:* ${total_pkb_pokok}
 💸 *PKB SWD :* ${total}
 📆 *Jatuh Tempo:* ${tgl_jatuh_tempo}
+⛽️ *Samsat Terdekat:* ${lokasi_samsat}
 `      
 //⛽️ *Rincian:* ${rincian}
 //  conn.reply(m.chat, `${res.result}`, m)
