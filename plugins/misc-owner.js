dbf651479101c3665e7c66eef2c30d0efcb5cb6e
Burhan Syam <burhansyam85@gmail.com>
2022-07-13 16:33:22 +0700

    Update misc-owner.js

--- plugins/misc-owner.js
+++ plugins/misc-owner.js
@@ -8,10 +8,18 @@ import PhoneNumber from 'awesome-phonenumber'
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:; @burhansyam;;;'
-                    + 'FN: Burhan Syam\n' // full name
-                    + 'ORG:AgenDosa;\n' // the organization of the contact
+                    + 'N:Syam;Burhan;;;\n'
+                    + 'FN:Burhan Syam\n' // full name
+                    + 'ORG:Agen Dosa .Inc;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6281808032100:+62 818-0803-2100\n' // WhatsApp ID + phone number
+                    + 'item1.TEL;\n'
+                    + 'item1.X-ABLabel:Burhan\n'
+                    + 'item2.EMAIL;type=INTERNET:me@burhansyam.com\n'
+                    + 'item2.X-ABLabel:Email\n'
+                    + 'item3.URL:https://www.burhansyam.com/\n'
+                    + 'item3.X-ABLabel:Web\n'
+                    + 'item4.ADR:;;Indonesia;;;;\n'
+                    + 'item4.X-ABLabel:Region\n'                
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'burhansyam', contacts: [{ vcard }] } }, { quoted: m })
 }
