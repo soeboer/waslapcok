a61eb23d82c44a793f1c44faa9d4abda782f9905
Burhan Syam <burhansyam85@gmail.com>
2022-07-14 08:11:51 +0700

    Create info-cekpdamgk.js

---
+++ plugins/info-cekpdamgk.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, text }) => {
+// let [nopol, teks] = text.split ` `
+
+    if (!text) return conn.reply(m.chat, '[❗INFO❗] Berapa ID Sambungannya ?\n Contoh : .cekpdamgk 050200292', m)
+
+  await m.reply('Sabar bestie saya cek dulu...')
+ let res = await fetch(`https://api.burhansyam.com/bot/pdamgk/?id=${text}`)
+let json = await res.json()
+let { pesan, nosamw, nama, alamat, urstat_smb, urjlw, periode, stand_k, stand_l, pakai, lembar, jml_tag, denda, subtotal, message } = json
+let pampers = `*Informasi Tagiahan PDAM Gunungkidul
+🚧 *No Sambungan    :* ${nosamw}
+⛽️ *Nama Pelanggan  :* ${nama}
+🛵 *Alamat :* ${alamat}
+🚏 *Periode       :* ${periode}
+🚧 *Status    :* ${urstat_smb}
+💰 *Beban pakai  :* ${stand_k} - ${stand_l} = ${pakai} M3
+💵 *Beban Pokok :* ${jml_tag}
+💵 *Denda :* ${denda}
+💸 *Wajib Bayar :* ${subtotal}
+📆 *Catatan :* \n${message}`
+  
+//  conn.reply(m.chat, `${res.result}`, m)
+           conn.reply(m.chat, pampers, m)
+}
+
+handler.help = ['cekpdamgk <No Sambungan>']
+handler.tags = ['info']
+handler.command = /^(cekpdamgk)$/i
+
+export default handler
