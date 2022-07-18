f71076be5e31f8847ed1fd7d9037fb60adb7a4cd
Burhan Syam <burhansyam85@gmail.com>
2022-07-18 14:50:05 +0700

    Update cari-joox.js

--- plugins/cari-joox.js
+++ plugins/cari-joox.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
-import cheerio from 'cheerio'
+
 let handler = async (m, { conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Judul Lagu Yang Ingin Kamu Cari*`
 await m.reply('siap saya carikan dulu bestie...')
-let res = await conn.getFile(`https://violetics.pw/api/media/joox-play?apikey=beta&query=${text}`)
+let res = await fetch(`https://violetics.pw/api/media/joox-play?apikey=beta&query=${text}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { malbum, msinger, mp3Url, msong, public_time, imgSrc } = json.results
