286e1352451daf99f4071eb77479d5b9df0d539b
Burhan Syam <burhansyam85@gmail.com>
2022-07-18 14:38:57 +0700

    Create cari-joox.js

---
+++ plugins/cari-joox.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+let handler = async (m, { conn, text }) => {
+if (!text) throw `*[❗INFO❗] Masukan Judul Lagu Yang Ingin Kamu Cari*`
+await m.reply('siap kirim...')
+let sound = await conn.getFile(`https://violetics.pw/api/media/joox-play?apikey=beta&query=${pilih}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+let { malbum, msinger, mp3Url, msong, public_time, imgSrc } = json.results
+
+		let caption = `*Lagu*\n\nFormat : ${text}`
+                conn.sendFile(m.chat, mp3Url, `${msong}.mp3`, caption, m)
+}
+handler.help = ['joox <lagu>']
+handler.tags = ['pencarian']
+handler.command = /^(joox)$/i
+export default handler
