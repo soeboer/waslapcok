c5e83498ec3fdc43d1c619dc7ddb5ab6cfa8c763
Burhan Syam <burhansyam85@gmail.com>
2022-07-21 03:16:22 +0700

    Update islami-daftarsurah.js

--- plugins/islami-daftarsurah.js
+++ plugins/islami-daftarsurah.js
@@ -1,15 +1,13 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
 
         let res = await fetch('https://api.quran.sutanlab.id/surah')
         let json = await res.json()
         let list_id = json.data.map((v, i) => `${i + 1}. ${v.name.transliteration.id}`).join('\n')
-        let list_en = json.data.map((v, i) => `${i + 1}. ${v.name.transliteration.en}`).join('\n')
-        let contoh = `contoh:\n*${usedPrefix}surah An-Nisaa 1*\n*${usedPrefix}ayta An-Nisaa 1*\n*${usedPrefix}tafsir An-Nisaa 1*\n*${usedPrefix}alquran 1 1*\n\nharus sesuai yang ada pada daftar surah dibawah ini, dan 1 aja ayatnya\n\n`
-        m.reply(contoh + list_en + '\n\n' + list_id)
-    } catch (e) {
-        throw `_*Error!*_`
+        let contoh = `contoh:\n*${usedPrefix}surah An-Nisaa 1*\n*${usedPrefix}ayat An-Nisaa 1*\n*${usedPrefix}tafsir An-Nisaa 1*\n*${usedPrefix}alquran 1 1*\n\nharus sesuai yang ada pada daftar surah dibawah ini, dan 1 aja ayatnya\n\n`
+        conn.reply(m.chat, contoh + list_id, m)
+    
     }
 
 handler.help = ['daftarsurah']
