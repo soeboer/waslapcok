51effa3d5c4b341e88d28e335bff9e3c919bee8a
Burhan Syam <burhansyam85@gmail.com>
2022-07-21 11:45:05 +0700

    Update cari-joox.js

--- plugins/cari-joox.js
+++ plugins/cari-joox.js
@@ -10,7 +10,7 @@ let { malbum, msinger, mp3Url, msong, public_time, imgSrc } = json.result
 		let caption = `*Lagu :* ${msong}\n*Album :* ${malbum}\n*Artis :* ${msinger}\n*Tahun :* ${public_time}`
  
 conn.sendFile(m.chat, mp3Url, `${msong}.mp3`, `${caption}`, m)
-	await conn.sendFile(m.chat, text, '', caption, m)
+conn.sendFile(m.chat, text, '', caption, m)
 }
 handler.help = ['joox <lagu>']
 handler.tags = ['pencarian']
