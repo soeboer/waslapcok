a48778d7a98c5563438c6b1a33f31c469b35ce71
Burhan Syam <burhansyam85@gmail.com>
2022-07-21 12:33:04 +0700

    Update cari-spotify.js

--- plugins/cari-spotify.js
+++ plugins/cari-spotify.js
@@ -5,8 +5,9 @@ if (!text) throw `*[❗INFO❗] Masukan Judul Lagu Yang Ingin Kamu Cari*`
 
 let res = await fetch(`https://api.zekais.com/spotify?apikey=zekais&query=${text}`)
 if (!res.ok) throw await res.text()
-let json = await res.json()
-let { title, thumb, result, artists, popularity, duration } = json
+// let json = await res.json()
+let { title, thumb, result, artists, popularity, duration } = await res.json()
+
 let spotipy = `✨ *Judul:* ${title}
 🎆 *Artis:* ${artists}
 💌 *Rating:* ${popularity}
