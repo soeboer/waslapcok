8d75e9a9ca6b15584cb256728c64e7c5a0202cfd
Burhan Syam <burhansyam85@gmail.com>
2022-07-21 12:42:41 +0700

    Update cari-joox.js

--- plugins/cari-joox.js
+++ plugins/cari-joox.js
@@ -7,7 +7,8 @@ let res = await fetch(`https://violetics.pw/api/media/joox-play?apikey=beta&quer
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { malbum, msinger, mp3Url, msong, public_time, imgSrc } = json.result
-		let caption = `*Lagu :* ${msong}\n*Album :* ${malbum}\n*Artis :* ${msinger}\n*Tahun :* ${public_time}`
+
+let caption = `*Lagu :* ${msong}\n*Album :* ${malbum}\n*Artis :* ${msinger}\n*Tahun :* ${public_time}`
  
 conn.sendFile(m.chat, mp3Url, `${msong}.mp3`, `${caption}`, m)
 conn.sendFile(m.chat, text, '', caption, m)
