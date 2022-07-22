8468cc3277fc3aa5682b57a09fe805262300fff9
Burhan Syam <burhansyam85@gmail.com>
2022-07-22 23:59:14 +0700

    Update info-cekqraset.js

--- plugins/info-cekqraset.js
+++ plugins/info-cekqraset.js
@@ -3,28 +3,30 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Kode QR Aset* \n Contoh : .cekqr 201700667583`
   await m.reply('Kata Mas Angga mau di cek dulu...')
- let res = await fetch(`https://aset.simda.net/2022/ai_cha/?qrcode=${text}`)
-if (!res.ok) throw await res.text()
+//  let res = await fetch(`https://aset.simda.net/2022/ai_cha/?qrcode=${text}`)
+ let res = await fetch(`https://api.burhansyam.com/bot/aset/?qrcode=${text}`)
+
+  if (!res.ok) throw await res.text()
 let json = await res.json()
 let { qrcode, kodekib, namabarang, tahunperolehan, nilaibarang, kodebarang, kodeopd, opd, status, kodelokasi, koderuang, ruang, kodeopdruang, penanggungjawab, lokasi } = json.result
 
 let asetku = `
 🏷 *Kode QR    :* ${qrcode}
 📃 *Kode KIB   :* ${kodekib}
-📄 *Nama Barang   :* ${namabarang}
-💰 *Nilai Barang:* ${nilaibarang}
+📄 *Nama Barang :* ${namabarang}
+💰 *Nilai Barang:* Rp ${nilaibarang}
 📆 *Tahun Perolehan :* ${tahunperolehan}
 🏠 *Nama OPD :* ${opd}
 💻 *Status Barang :* ${status}
 🪙 *Kode Barang :* ${kodebarang}
 🚧 *Ruang :* ${ruang}
 🚏 *Kode Ruang :* ${kodeopdruang}
-🚨 *Kode OPD Ruang :* ${kodeopdruang}
 🖨 *Kode Lokasi :* ${kodelokasi}
 🧑🏿‍✈️ *Penanggung Jawab:* ${penanggungjawab}
 🗺 *Lokasi Barang:* \n${lokasi}`      
            conn.reply(m.chat, asetku, m)
 }
+// 🚨 *Kode OPD Ruang :* ${kodeopdruang}
 
 handler.help = ['cekqr <kodeqr aset>']
 handler.tags = ['info']
