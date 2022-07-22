05e8fbb26a84a8ac19a2248b6e20fd55721b630c
Burhan Syam <burhansyam85@gmail.com>
2022-07-22 15:15:42 +0700

    Update and rename maker-colorful.js to maker-sunset.js

--- plugins/maker-sunset.js
+++ plugins/maker-sunset.js
@@ -4,15 +4,15 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, usedPrefix, command}) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}wait`
+  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}sunset`
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
     let url = await uploadImage(img)
     await m.reply('ditunggu dulu kak...')
 
-let gbr = await conn.getFile(`https://violetics.pw/api/photofilter/warm-sunset?apikey=beta&image=${url}`)
- 
-  conn.sendFile(m.chat, gbr.data, 'ihik.jpg', `Tukang Edit Manual`, m, false)
+  let res = `https://violetics.pw/api/photofilter/warm-sunset?apikey=beta&image=${url}
+  conn.sendFile(m.chat, res, 'sunset.jpg', `Efek Sunset`, m, false)
+
  
 }
  
