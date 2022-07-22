27b75d08fb81f7ba048a7f04c90a6161ee3b79ae
Burhan Syam <burhansyam85@gmail.com>
2022-07-22 17:41:42 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,18 +1,17 @@
-// import * as scraper from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
     if (!text) throw 'Masukkan Alamat URL'
-//     let res = await scraper.instagramdl(args[0])
-//         .catch(async _ => await scraper.instagramdlv2(args[0]))
-//         .catch(async _ => await scraper.instagramdlv3(args[0]))
-//         .catch(async _ => await scraper.instagramdlv4(args[0]))
-    let res = await fetch(`https://api.zekais.com/igdl2?apikey=zekais&url=${text}`)
-    let json = await res.json()
-//     if (!res) throw 'Can\'t download the post'
 
+let res = await fetch(`https://api.zekais.com/igdl2?apikey=zekais&url=${text}`)
+if (!res.ok) throw await res.text()
+    
     await m.reply('_Dalam proses, mohon ditunggu..._')
-    for (let { url } of json.result[0]) await m.conn.sendFile(m.chat, url, '', '', m)
+let json = await res.json()
+let { url } = json.result[0]
+let pidio = await conn.getFile(`${url}`)
+		let caption = `@burhansyam`
+                conn.sendFile(m.chat, pidio.data, `get.${pidio}.mp4`, caption, m)
 }
 handler.help = ['instagram']
 handler.tags = ['downloader']
