9891f073e04e05482fa6ea919afae1e0671ab6fd
Burhan Syam <burhansyam85@gmail.com>
2022-07-24 18:03:33 +0700

    Create downloader-cocofun.js

---
+++ plugins/downloader-cocofun.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw 'Masukkan Alamat URL'
+
+let res = await fetch(`https://docs-jojo.herokuapp.com/api/cocofun-no-wm?url=${text}`)
+if (!res.ok) throw await res.text()
+    
+    await m.reply('_Dalam proses, mohon ditunggu..._')
+let json = await res.json()
+let { download } = json
+let pidio = await conn.getFile(`${download}`)
+		let caption = `@burhansyam`
+                conn.sendFile(m.chat, pidio.data, `get.${pidio}.mp4`, caption, m)
+}
+handler.help = ['cocofun']
+handler.tags = ['downloader']
+handler.command = /^(coco(fun)?)$/i
+
+export default handler
