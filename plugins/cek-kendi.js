acd5fcbc5f19cbd8c5d837fb3d4e909b6db968db
Burhan Syam <burhansyam85@gmail.com>
2022-07-25 12:04:50 +0700

    Create cek-kendi.js

---
+++ plugins/cek-kendi.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch'
+
+let handler = async(m, { conn, text }) => {
+if (!text) throw `*[❗INFO❗] Masukan Kode QR Aset* \n Contoh : .cekqr 201700667583`
+  await m.reply('🕵️ Sabar, Kata Mas Angga mau di cek dulu...')
+//  let res = await fetch(`https://aset.simda.net/2022/ai_cha/?qrcode=${text}`)
+ let res = await fetch(`https://api.burhansyam.com/bot/aset/?nopol=${text}`)
+
+  if (!res.ok) throw await res.text()
+let json = await res.json()
+let { qrcode, kodekib, namabarang, pkb, merk, model, tahunperolehan, nilaibarang, kodebarang, kodeopd, opd, status, kodelokasi, koderuang, ruang, kodeopdruang, penanggungjawab, lokasi, tglstnk,  } = json.result
+
+let asetku = `*Detail Barang dalam SIM ASET :*
+📄 *Nomor Polisi :* ${text}
+📦 *Jenis :* ${namabarang}
+🚨 *Merk :* ${merk}
+📄 *Model :* ${model}
+💰 *Nilai Barang :* Rp ${nilaibarang}
+📆 *Tahun Perolehan :* ${tahunperolehan}
+🏠 *Nama OPD :* ${opd}
+💻 *Status Barang :* ${status}
+📲 *Kode QR    :* ${qrcode}
+📃 *Akhir Pajak   :* ${tglstnk}
+💵 *Pajak Kendaraan :* ${pkb}
+💾 *Kode Barang :* ${kodebarang}
+🚏 *Ruang :* ${ruang}
+🧑‍✈️ *Penanggung Jawab :* ${penanggungjawab}
+🌎 *Lokasi Barang :* \n${lokasi}`      
+           conn.reply(m.chat, asetku, m)
+}
+// 🚨 *Kode OPD Ruang :* ${kodeopdruang}
+// 📄 *Kode Ruang :* ${kodeopdruang}
+// 🗺 *Kode Lokasi :* ${kodelokasi}
+
+handler.help = ['kendi <AB 1056 UD>']
+handler.tags = ['info']
+handler.command = /^(kendi)$/i
+
+export default handler
