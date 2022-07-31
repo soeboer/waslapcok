408d3e1c8305d248308b54f92912acf67ae8114c
Burhan Syam <burhansyam85@gmail.com>
2022-07-31 11:35:24 +0700

    Update _tes.js

--- plugins/_tes.js
+++ plugins/_tes.js
@@ -1,12 +1,12 @@
 import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-          let hasil = `Waah situ hebat yak dah berani ngetes ngetes??? `
+          let hasil = `Waalaikumsalam warahmatullahi wabarakatuh\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ\nSiap bisa dibantu?\nUntuk bantuan silakan ketik .bkad`
             conn.reply(m.chat, hasil, m)
     
 }
 
-handler.customPrefix = /^(tes(t)?|p)$/i
+handler.customPrefix = /^(ass(alam)?|p)$/i
 handler.command = new RegExp
 
 export default handler
