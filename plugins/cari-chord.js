f33482a387c7e7095e2c4f814432d6a8a03d53e2
Burhan Syam <burhansyam85@gmail.com>
2022-07-31 10:11:23 +0700

    Update cari-chord.js

--- plugins/cari-chord.js
+++ plugins/cari-chord.js
@@ -3,9 +3,11 @@ import axios from 'axios'
 let handler = async(m, { conn, text, usedPrefix }) => {
 
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'chord hanya rindu', m)
-    axios.get(`https://hadi-api.herokuapp.com/api/chord/?q=` + text)
+//     axios.get(`https://hadi-api.herokuapp.com/api/chord/?q=` + text)
+    axios.get(`https://api.burhansyam.com/bot/chord/?q=` + text)    
+    
         .then((res) => {
-          let hasil = `*• Chord Lagu ${text} :*\n${res.data.result}`
+          let hasil = `*• Chord Lagu ${text} :*\n${res.data.posts.content}`
             conn.reply(m.chat, hasil, m)
         })
         .catch(_ => m.reply('Chord Lagu Tidak Ditemukan!'))
