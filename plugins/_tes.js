13a549d71ab60ff6e2a16294ec5d0ec218b39dbd
Burhan Syam <burhansyam85@gmail.com>
2022-07-31 11:42:00 +0700

    Update _tes.js

--- plugins/_tes.js
+++ plugins/_tes.js
@@ -1,7 +1,7 @@
 // import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-          let hasil = `Waalaikumsalam warahmatullahi wabarakatuh\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ\nSiap bisa dibantu?\nUntuk bantuan silakan ketik .bkad`
+          let hasil = `Waalaikumsalam warahmatullahi wabarakatuh\nSiap bisa dibantu?\nUntuk bantuan silakan ketik .bkad`
             conn.reply(m.chat, hasil, m)
     
 }
