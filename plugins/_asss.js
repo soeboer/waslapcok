7dd897b731826f2c06ed951a3e2ba1b9fd986ab9
Burhan Syam <burhansyam85@gmail.com>
2022-07-31 12:18:48 +0700

    Create _asss.js

---
+++ plugins/_asss.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+          let hasil = `Waalaikumsalam warahmatullahi wabarakatuh\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ\nSiap bisa dibantu?\nUntuk bantuan silakan ketik .bkad`
+            conn.reply(m.chat, hasil, m)
+
+}
+
+handler.customPrefix = /^(ass(alam)?|p)$/i
+handler.command = new RegExp
+
+export default handler
