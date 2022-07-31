73c8fb30d600d7157aa354f9016ae409ce95c477
Burhan Syam <burhansyam85@gmail.com>
2022-07-31 10:55:55 +0700

    Update cari-kuncigitar.js

--- plugins/cari-kuncigitar.js
+++ plugins/cari-kuncigitar.js
@@ -6,13 +6,12 @@ let res = await fetch(global.API('https://api.burhansyam.com', '/bot/chord/', { 
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { id, type, slug, url, status, title, title_plain, image_url, content } = json.posts[0]
-
-let koenci = `✨ *Judul:* ${title}
+let koenci = `*Judul:* ${title}
 ${content}`
 // conn.sendFile(m.chat, image_url, '', animeingfo, m)
             conn.reply(m.chat, koenci, m)
-
 }
+
 handler.help = ['kuncigitar <lagu>']
 handler.tags = ['pencarian']
 handler.command = /^(kuncigitar)$/i
