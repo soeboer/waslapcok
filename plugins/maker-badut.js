759a244e11ae057d3ae2aca5b0577a59503d6fb0
Burhan Syam <burhansyam85@gmail.com>
2022-08-02 03:22:07 +0700

    Update maker-badut.js

--- plugins/maker-badut.js
+++ plugins/maker-badut.js
@@ -1,13 +1,15 @@
 import fetch from 'node-fetch'
-import uploadImage from '../lib/uploadImage.js'
+// import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix}) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}.vector`
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
-    let img = await q.download()
-    let urle = await uploadImage(img)
+//     let img = await q.download()
+//     let urle = await uploadImage(img)
+        let urle = await webp2png(await q.download())
     await m.reply('Sabar bestie,dibuatkan dulu yaak...')
 
  let res = await fetch(`https://api.burhansyam.com/bot/pho.to/badut/index.php?url=${urle}`)
