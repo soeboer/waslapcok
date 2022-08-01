1854c8d00ec1c8a1dab75c983f8def02c40b5142
Burhan Syam <burhansyam85@gmail.com>
2022-08-02 03:28:13 +0700

    Create maker-gorila.js

---
+++ plugins/maker-gorila.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+// import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn, usedPrefix}) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}.vector`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+//     let img = await q.download()
+//     let urle = await uploadImage(img)
+        let urle = await webp2png(await q.download())
+    await m.reply('Sabar bestie,dibuatkan dulu yaak...')
+
+ let res = await fetch(`https://api.burhansyam.com/bot/pho.to/gorila/index.php?url=${urle}`)
+ 
+ 	if (res.status !== 200) throw res.statusText
+	conn.sendMessage(m.chat, { image: { url: res.url }}, { quoted: m })
+
+}
+handler.help = ['gorila']
+handler.tags = ['maker']
+handler.command = /^(gorila)$/i
+handler.fail = null
+
+
+export default handler
