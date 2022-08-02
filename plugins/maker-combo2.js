3ff621f35dba0266f8c654b53ef2e5d5af1e933a
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 03:46:01 +0700

    Update and rename maker-combo3.js to maker-combo2.js

--- plugins/maker-combo2.js
+++ plugins/maker-combo2.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, usedPrefix}) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}.vector`
+  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption`
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
     let urle = await uploadImage(img)
@@ -16,9 +16,9 @@ let handler = async (m, { conn, usedPrefix}) => {
 	conn.sendMessage(m.chat, { image: { url: res.url }}, { quoted: m })
 
 }
-handler.help = ['combo3']
+handler.help = ['combo2']
 handler.tags = ['maker']
-handler.command = /^(combo3)$/i
+handler.command = /^(combo2)$/i
 handler.fail = null
 
 
