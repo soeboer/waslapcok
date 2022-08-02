0ab68f0c732726db7eb0fe37dafe353c9ef20e95
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 03:42:19 +0700

    Update and rename maker-vector3.js to maker-combo1.js

--- plugins/maker-combo1.js
+++ plugins/maker-combo1.js
@@ -4,21 +4,21 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, usedPrefix}) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}.vector`
+  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption`
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
     let urle = await uploadImage(img)
     await m.reply('Sabar bestie,dibuatkan dulu yaak...')
 
- let res = await fetch(`https://api.burhansyam.com/bot/pho.to/vkuning/index.php?url=${urle}`)
+ let res = await fetch(`https://api.burhansyam.com/bot/pho.to/combo1/index.php?url=${urle}`)
  
  	if (res.status !== 200) throw res.statusText
 	conn.sendMessage(m.chat, { image: { url: res.url }}, { quoted: m })
 
 }
-handler.help = ['vector3']
+handler.help = ['combo1']
 handler.tags = ['maker']
-handler.command = /^(vector3)$/i
+handler.command = /^(combo1)$/i
 handler.fail = null
 
 
