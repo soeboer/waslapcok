3cf0ff935b7971f9170261349480f823dd142fbd
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 04:22:07 +0700

    Create maker-brewokputih.js

---
+++ plugins/maker-brewokputih.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, usedPrefix}) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+    let img = await q.download()
+    let urle = await uploadImage(img)
+    await m.reply('Sabar bestie,dibuatkan dulu yaak...')
+
+ let res = await fetch(`https://api.burhansyam.com/bot/pho.to/brewokputih/index.php?url=${urle}`)
+ 
+ 	if (res.status !== 200) throw res.statusText
+	conn.sendMessage(m.chat, { image: { url: res.url }}, { quoted: m })
+
+}
+handler.help = ['brewokputih']
+handler.tags = ['maker']
+handler.command = /^(brewokputih)$/i
+handler.fail = null
+
+
+export default handler
