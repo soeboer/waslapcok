d273ef59f2046c47375bec8956c46b633c86f931
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 12:50:38 +0700

    Create -araissabd.js

---
+++ plugins/-araissabd.js
@@ -0,0 +1,22 @@
+// let handler = async (m, { conn, text, usedPrefix, command }) => {
+//           let hasil = `Waalaikumsalam warahmatullahi wabarakatuh\nوَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ\nSiap bisa dibantu?\nUntuk bantuan silakan ketik .bkad`
+//             conn.reply(m.chat, hasil, m)
+
+// }
+import moment from 'moment-timezone'
+
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
+let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+let bales = `Baik terimakasih banyak ya,saya tunggu dirumah`
+// conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ\nˢᵉˡᵃᵐᵃᵗ ᴮᵉᵏᵉʳʲᵃ\nˢᵃʸᵃ ᵃᵈᵃˡᵃʰ ᵐᵉˢⁱⁿ ᵖᵉⁿʲᵃʷᵃᵇ ʸᵃⁿᵍ ᵈⁱᵇᵘᵃᵗ ᵒˡᵉʰ @ᵇᵘʳʰᵃⁿˢʸᵃᵐ ᵘⁿᵗᵘᵏ ᵐᵉᵐᵇᵃⁿᵗᵘ ᵐᵉʳⁱⁿᵍᵃⁿᵏᵃⁿ ᵗᵘᵍᵃˢ ᵈᵃʳⁱ ᴮᴷᴬᴰ ᵈⁱ ˡᵃᵖᵃⁿᵍᵃⁿ.ᴶⁱᵏᵃ ᵃᵈᵃ ᵏʳⁱᵗⁱᵏ ᵈᵃⁿ ˢᵃʳᵃⁿ ˢⁱˡᵃᵏᵃⁿ ᵏⁱʳⁱᵐ ᵏᵉ @ᵇᵘʳʰᵃⁿˢʸᵃᵐ ᵈⁱ ᵀᴵᴮᴷᴬᴰ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ 𝐁𝐊𝐀𝐃, ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'burhansyam.com', 'ᴡᴇʙ', null, null, [
+// [`𝐁𝐊𝐀𝐃`, '.bkad']
+// ], m, {asLocation: true})
+    m.reply(bales)
+}
+
+
+handler.customPrefix = /^(icha)?|hbd|halo)$/i
+handler.command = new RegExp
+
+export default handler
