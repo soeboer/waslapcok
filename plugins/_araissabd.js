58446f047c0b0de7617cf0116a20def382ec81d4
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 13:53:04 +0700

    Update _araissabd.js

--- plugins/_araissabd.js
+++ plugins/_araissabd.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text }) => {
-let [teks] = text.split ` `
-if (!teks) return conn.reply(m.chat, 'yaa bisa dibantu ?', m)
+
 let bales = `Halo..Terimakasih banyak ucapan & doanya,saya tunggu dirumah yaaa 😊 😍 🥰 😘`
             conn.reply(m.chat, bales, m)
 }
 
 
-handler.customPrefix = /^(icha|hbd|halo|selamat|hpbd|dek)$/i
+// handler.customPrefix = /^(icha|hbd|halo|selamat|hpbd|dek)$/i
+/(h?(a|o)i?|icha|hbd|hpbd|selamat)/i
 handler.command = new RegExp
 
 export default handler
