b0b0dfb48543e4315ddf1ee15d7c5064cb5cc623
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 22:11:44 +0700

    Update general_responbkad.js

--- plugins/general_responbkad.js
+++ plugins/general_responbkad.js
@@ -6,13 +6,13 @@ import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ\n *${wish()}*\nˢᵃʸᵃ ᵃᵈᵃˡᵃʰ ᵐᵉˢⁱⁿ ᵖᵉⁿʲᵃʷᵃᵇ ʸᵃⁿᵍ ᵈⁱᵇᵘᵃᵗ ᵘⁿᵗᵘᵏ ᵐᵉᵐᵇᵃⁿᵗᵘ ᵐᵉʳⁱⁿᵍᵃⁿᵏᵃⁿ ᵗᵘᵍᵃˢ ᵈᵃʳⁱ ᴮᴷᴬᴰ ᵈⁱ ˡᵃᵖᵃⁿᵍᵃⁿ.ᴶⁱᵏᵃ ᵃᵈᵃ ᵏʳⁱᵗⁱᵏ ᵈᵃⁿ ˢᵃʳᵃⁿ ˢⁱˡᵃᵏᵃⁿ ᵏⁱʳⁱᵐ ᵏᵉ @ᵇᵘʳʰᵃⁿˢʸᵃᵐ ᵈⁱ ᵀᴵᴮᴷᴬᴰ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ 𝐁𝐊𝐀𝐃, ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://bkad.gunungkidulkab.go.id/', 'ᴡᴇʙ', null, null, [
+conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `${wish()}\nوَعَلَيْكُمُ السَّلَامُ \nᴬᵈᵃ ʸᵃⁿᵍ ᵇⁱˢᵃ ᵈⁱᵇᵃⁿᵗᵘ?ˢᵃʸᵃ ᵃᵈᵃˡᵃʰ ᵐᵉˢⁱⁿ ᵖᵉⁿʲᵃʷᵃᵇ ʸᵃⁿᵍ ᵈⁱᵇᵘᵃᵗ ᵘⁿᵗᵘᵏ ᵐᵉᵐᵇᵃⁿᵗᵘ ᵐᵉʳⁱⁿᵍᵃⁿᵏᵃⁿ ᵗᵘᵍᵃˢ ᵈᵃʳⁱ ᴮᴷᴬᴰ ᵈⁱ ˡᵃᵖᵃⁿᵍᵃⁿ.ᴶⁱᵏᵃ ᵃᵈᵃ ᵏʳⁱᵗⁱᵏ ᵈᵃⁿ ˢᵃʳᵃⁿ ˢⁱˡᵃᵏᵃⁿ ᵏⁱʳⁱᵐ ᵏᵉ @ᵇᵘʳʰᵃⁿˢʸᵃᵐ ᵈⁱ ᵀᴵᴮᴷᴬᴰ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ 𝐁𝐊𝐀𝐃, ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://bkad.gunungkidulkab.go.id/', 'ᴡᴇʙ', null, null, [
 [`𝐁𝐊𝐀𝐃`, '.bkad']
 ], m, {asLocation: true})
 }
 
 
-handler.customPrefix = /^ass?alam|mas|mbak|pak|bu|permisi|tes|mohon|pagi|siang|sore|malam$/i
+handler.customPrefix = /^assalamualaikum|salam|permisi|tes|mohon|bkad$/i
 handler.command = new RegExp
 
 export default handler
@@ -21,16 +21,16 @@ function wish() {
   const time = moment.tz('Asia/Jakarta').format('HH')
   let wishloc = "Selamat Dini Hari ☀️"
   if (time >= 4) {
-    wishloc = "Selamat Pagi 🌄"
+    wishloc = "*Selamat Pagi* 🌄"
   }
   if (time >= 10) {
-    wishloc = "Selamat Siang ☀️"
+    wishloc = "*Selamat Siang* ☀️"
   }
   if (time >= 15) {
-    wishloc = "Selamat Sore 🌇"
+    wishloc = "*Selamat Sore* 🌇"
   }
   if (time >= 18) {
-    wishloc = "Selamat Malam 🌙"
+    wishloc = "*Selamat Malam* 🌙"
   }
   return wishloc
 }
