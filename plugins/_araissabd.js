ceeb99556401fb8bbece6591190446469477ce1a
Burhan Syam <burhansyam85@gmail.com>
2022-08-03 13:39:32 +0700

    Update _araissabd.js

--- plugins/_araissabd.js
+++ plugins/_araissabd.js
@@ -1,4 +1,6 @@
 let handler = async (m, { conn, text }) => {
+let [teks] = text.split ` `
+if (!teks) return conn.reply(m.chat, 'yaa bisa dibantu ?', m)
 let bales = `Halo..Terimakasih banyak ucapan & doanya,saya tunggu dirumah yaaa 😊 😍 🥰 😘`
             conn.reply(m.chat, bales, m)
 }
