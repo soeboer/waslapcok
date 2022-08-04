4a50b8aaf99d8e488029be6011c482dd0eb3f25e
Burhan Syam <burhansyam85@gmail.com>
2022-08-04 22:57:47 +0700

    Create tools-uploadfoto.js

---
+++ plugins/tools-uploadfoto.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, usedPrefix}) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+    let img = await q.download()
+    let urle = await uploadImage(img)
+    await m.reply('Sabar bestie,saya Upload dulu yaak...')
+  
+let peroses = `🎆 *Proses Upload Selesai...*
+🌐 *Link Gambar*: ${urle}`
+
+conn.sendFile(m.chat, img, '', peroses, m)
+  
+  
+}
+handler.help = ['brewokputih']
+handler.tags = ['maker']
+handler.command = /^(brewokputih)$/i
+handler.fail = null
+
+
+export default handler
