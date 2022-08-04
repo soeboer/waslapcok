d6526685aba391a70f30744557c6c940e1123477
Burhan Syam <burhansyam85@gmail.com>
2022-08-04 11:16:58 +0700

    Update cari-loker.js

--- plugins/cari-loker.js
+++ plugins/cari-loker.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-//     if (!text) throw `*Perintah ini untuk mencari kode pos berdasarkan kota/pencarian*\n\ncontoh:\n${usedPrefix + command} Ponjong`
-    let res = await fetch(global.API('https://docs-jojo.herokuapp.com', '/api/infoloker'))
-//     if (res.status != 200) throw await res.text()
+// let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+let handler = async(m, { conn }) => {
+await m.reply('saya carikan dulu kak...')
+let res = await fetch(global.API('https://docs-jojo.herokuapp.com', '/api/infoloker'))
     let json = await res.json()
-    if (!json.status) throw json
-    let mes = json.result.map((v, i) => `${i + 1}.🏭 Perusahaan: ${v.perusahaan}\n👷🏽‍♂️ Profesi: ${v.profesi}\n🧑🏽‍🎓 Pendidikan: ${v.edukasi}\n🗺 Lokasi: ${v.lokasi}\n👷🏽‍♂️ Bagian: ${v.jobFunction}\n📈 Karier: ${v.levelKarir}\n👨🏽‍💻 Desc Job: ${v.desc}\n🚧 Persyaratan: ${v.syarat}\n💵 Gaji: ${v.gaji}\n🌐 Info Lamaran: ${v.link}`).join('\n\n')
+    let mes = json.result.map((v, i) => `${i + 1}. 🏭 Perusahaan: ${v.perusahaan}\n👷🏽‍♂️ Profesi: ${v.profesi}\n🧑🏽‍🎓 Pendidikan: ${v.edukasi}\n🗺 Lokasi: ${v.lokasi}\n👷🏽‍♂️ Bagian: ${v.jobFunction}\n📈 Karier: ${v.levelKarir}\n👨🏽‍💻 Desc Job: ${v.desc}\n🚧 Persyaratan: ${v.syarat}\n💵 Gaji: ${v.gaji}\n🌐 Info Lamaran: ${v.link}`).join('\n')
     m.reply(mes)
 //     	          conn.reply(m.chat, mes, m)
 
