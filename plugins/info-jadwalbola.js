b025584688e0cef5fa3cdcf9d8f6b60a01fcdc6f
Burhan Syam <burhansyam85@gmail.com>
2022-08-05 00:16:19 +0700

    Update info-jadwalbola.js

--- plugins/info-jadwalbola.js
+++ plugins/info-jadwalbola.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, {command, conn}) => {
      let res = await fetch(global.API('https://docs-jojo.herokuapp.com', '/api/jadwal-bola'))
      let json = await res.json()
-     let bol = json.result.map((v, i) => `${i + 1}.📆 Tanggal : ${v.tanggal}\n⌚️ Jam : ${v.jam}\n📊 Liga : ${v.liga}\n⚽️ Pertandingan : ${v.match}\n📺 Channel : ${v.ch_tv}`).join('\n\n')
+     let bol = json.map((v, i) => `${i + 1}.📆 Tanggal : ${v.tanggal}\n⌚️ Jam : ${v.jam}\n📊 Liga : ${v.liga}\n⚽️ Pertandingan : ${v.match}\n📺 Channel : ${v.ch_tv}`).join('\n\n')
      m.reply(bol)
 }
 handler.help = ['jadwalbola']
