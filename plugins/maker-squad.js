03fe358a3ea9230848d549fffc895216aa98eb2d
Burhan Syam <burhansyam85@gmail.com>
2022-08-05 10:36:41 +0700

    Update maker-squad.js

--- plugins/maker-squad.js
+++ plugins/maker-squad.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 // let response = args.join(' ').split(' ')
-if (!text) throw `*[❗INFO❗] Masukan Nama Squad contoh .squad Agen Dosa Inc`
+if (!text) throw `*[❗INFO❗] Masukan Nama Squad contoh\n.squad Agen Dosa Inc`
   m.reply('Sabar kak, saya proses dulu yaak..')
   let res = `https://docs-jojo.herokuapp.com/api/gaming?text=${text}`
   conn.sendFile(m.chat, res, 'gaming.jpg', `@burhansyam Logo Maker`, m, false)
