8ec40bd27f999d1c2c0783dd47880d5427b28446
Burhan Syam <burhansyam85@gmail.com>
2022-08-05 11:30:36 +0700

    Update and rename maker-michat.js to maker-anya.js

--- plugins/maker-anya.js
+++ plugins/maker-anya.js
@@ -10,15 +10,15 @@ let handler = async (m, { conn, usedPrefix}) => {
     let urle = await uploadImage(img)
     await m.reply('Sabar bestie,dibuatkan dulu yaak...')
 
- let res = await fetch(`https://api.burhansyam.com/bot/pho.to/michat/index.php?url=${urle}`)
+ let res = await fetch(`https://api.burhansyam.com/bot/pho.to/anya/index.php?url=${urle}`)
  
  	if (res.status !== 200) throw res.statusText
 	conn.sendMessage(m.chat, { image: { url: res.url }}, { quoted: m })
 
 }
-handler.help = ['bgbo']
+handler.help = ['bganya']
 handler.tags = ['maker']
-handler.command = /^(bgbo)$/i
+handler.command = /^(bganya)$/i
 handler.fail = null
 
 
