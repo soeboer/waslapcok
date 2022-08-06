bc487c20350157bb27933c4a6a2e712545c7e360
Burhan Syam <burhansyam85@gmail.com>
2022-08-06 16:04:14 +0700

    Update bkad-mobsi.js

--- plugins/bkad-mobsi.js
+++ plugins/bkad-mobsi.js
@@ -5,7 +5,7 @@ let ihik = args.join(' ').split(' ')
   if (!args[0]) throw 'Masukkan Perintah dan Dev ID \nContoh: .mobsi pulang 256719cfac87abf0'
  await m.reply('Siap bestie,saya otewe mencet tombol ke BKPPD dulu 😅')
  let res = await fetch(`https://api.burhansyam.com/bot/mobsi/?ayo=${ihik[0]}&id=${ihik[1]}`)
-if (!res.ok) throw await res.text()
+// if (!res.ok) throw await res.text()
 let json = await res.json()
 let { mobsi, status, waktu, nama, nip, opd, jarak, koordinat } = json.result
 let kaslak = `🦉 *Rincian Presensi* ${mobsi}
@@ -20,7 +20,7 @@ let kaslak = `🦉 *Rincian Presensi* ${mobsi}
 
 conn.reply(m.chat, kaslak, m)
 }
-}
+
 handler.help = ['mobsi <Opsi DevId>']
 handler.tags = ['bkad']
 handler.command = /^(mobsi)$/i
