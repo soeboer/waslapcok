4a97e262c959d6cfb92dfa1bcdc7537690b70ace
Burhan Syam <burhansyam85@gmail.com>
2022-08-06 22:46:05 +0700

    Create downloader-xnxx.js

---
+++ plugins/downloader-xnxx.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `*Perintah ini untuk Download link XNXX *\n\ncontoh:\n${usedPrefix + command} https://www.xnxx.com/video-10795jab/sex_in_japan`
+    await m.reply('💻 Sabar bestie saya cek dulu...')
+    let res = await fetch(global.API('https://api.burhansyam.com', '/bot/xnxx/down/', { q: text }))
+    if (res.status != 200) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
+  let { title, url, duration, image, info, files, hls } = json.result
+  let kuali = json.files.map((v, i) => `${i + 1}.💾 SD: ${v.low}\n💽 HD: ${v.high}`).join('\n\n')
+  let ihik = `*Detail Video ${title}*
+🚧 *Link    :* ${url}
+⛽️ *Durasi  :* ${duration}
+📆 *Info    :* ${info}
+🚏 *Steaming :* ${hls}
+💰 *Downoad  :* \n${kuali}`
+  
+//     m.reply(papah)
+//     	          conn.reply(m.chat, mes, m)
+  conn.sendFile(m.chat, image, '', ihik, m)
+
+}
+
+handler.help = ['xd <link xnxx>']
+handler.tags = ['downloader']
+handler.command = /^xd/i
+export default handler
