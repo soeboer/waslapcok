eb8165eb0bfa06104d79254fd3ddb1642c03e787
Burhan Syam <burhansyam85@gmail.com>
2022-08-28 14:01:05 +0700

    Update cari-pelem.js

--- plugins/cari-pelem.js
+++ plugins/cari-pelem.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Judul Film Yang Ingin Kamu Cari*`
 await m.reply('saya carikan dulu bestie...')
-let res = await fetch(global.API('https://api.burhansyam.com', '/bot/pelem/', { q: text }))
+let res = await fetch(global.API('https://indotv.my.id', '/bot/pelem/', { q: text }))
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { title, thumb, views, genre, quality, country, duration, release, result } = json.result
