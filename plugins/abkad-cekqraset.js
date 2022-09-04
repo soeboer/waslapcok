d8bdcebbc1b54ff9bfc39996b012d40b9a78a203
Burhan Syam <burhansyam85@gmail.com>
2022-09-05 00:47:24 +0700

    Update abkad-cekqraset.js

--- plugins/abkad-cekqraset.js
+++ plugins/abkad-cekqraset.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Kode QR Aset* \n Contoh : .cekqr 201700667583`
   await m.reply('🕵️ Sabar, Kami cek di SIM ASET dulu...')
 //  let res = await fetch(`https://aset.simda.net/2022/ai_cha/?qrcode=${text}`)
- let res = await fetch(`https://indotv.my.id/bot/aset/?qrcode=${text}`)
+ let res = await fetch(`https://api.beetv.my.id/bot/aset/?qrcode=${text}`)
 
   if (!res.ok) throw await res.text()
 let json = await res.json()
