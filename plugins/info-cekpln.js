9e69a8e843f2cfa9031d915b9eeb929d1e807b79
Burhan Syam <burhansyam85@gmail.com>
2022-09-05 00:55:29 +0700

    Update info-cekpln.js

--- plugins/info-cekpln.js
+++ plugins/info-cekpln.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'ID Pelanggannya ? \n contoh : .cekpln 521050558080', m)
   await m.reply('Sabar bestie saya cek dulu...')
-let res = await fetch(`https://api.burhansyam.com/bot/pln/?id=${text}`)
+let res = await fetch(`https://api.beetv.my.id/bot/pln/?id=${text}`)
 let json = await res.json()
 let { bill_number, period, print } = json
 let jembat = `⚡️ *${print}*`      
