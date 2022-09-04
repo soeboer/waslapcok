7575ce79c8a78f5b278db30c14e113c49910fe30
Burhan Syam <burhansyam85@gmail.com>
2022-09-05 01:07:25 +0700

    Update cari-kuncigitar.js

--- plugins/cari-kuncigitar.js
+++ plugins/cari-kuncigitar.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
 if (!text) throw `*[❗INFO❗] Masukan Judul Lagu yang Kamu Cari*`
-let res = await fetch(global.API('https://indotv.my.id', '/bot/chord/', { q: text }))
+let res = await fetch(global.API('https://api.beetv.my.id', '/bot/chord/', { q: text }))
 if (!res.ok) throw await res.text()
 let json = await res.json()
 let { id, type, slug, url, status, title, title_plain, image_url, content } = json.posts[0]
