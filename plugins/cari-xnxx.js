f05e1400cc7d60bb5becdf34157efa2aee4151ec
Burhan Syam <burhansyam85@gmail.com>
2022-09-05 00:51:34 +0700

    Update cari-xnxx.js

--- plugins/cari-xnxx.js
+++ plugins/cari-xnxx.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `*Perintah ini untuk mencari film berdasar kata kunci judul/kategori*\ncontoh:\n${usedPrefix + command} abg`
     await m.reply('Sabar bestie saya carikan dulu...')
-    let res = await fetch(global.API('https://indotv.my.id', '/bot/xnxx/', { q: text }))
+    let res = await fetch(global.API('https://api.beetv.my.id', '/bot/xnxx/', { q: text }))
     let json = await res.json()
     let xxx = json.result.map((v, i) => `${i + 1}.💋 Judul : ${v.title}\n👙 Info : ${v.info}\n🤏🏼 Link : ${v.link}`).join('\n\n')
     m.reply(xxx)
