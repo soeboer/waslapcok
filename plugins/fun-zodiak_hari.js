14471e25ea1d65c8b86c548e09233f421ebb1205
Burhan Syam <burhansyam85@gmail.com>
2022-09-05 01:07:12 +0700

    Update fun-zodiak_hari.js

--- plugins/fun-zodiak_hari.js
+++ plugins/fun-zodiak_hari.js
@@ -38,7 +38,7 @@ ${usedPrefix + command} Taurus
 	    
 await m.reply('saya proses dulu kak...')
 
-let url = await fetch(global.API('https://api.burhansyam.com', '/bot/zodiak/', { q: pilih }))
+let url = await fetch(global.API('https://api.beetv.my.id', '/bot/zodiak/', { q: pilih }))
 // if (!res.ok) throw await res.pilih()
 let json = await url.json()
 let { image, hari_ini, description, angka_keberuntungan, peribahasa_cina, about } = json.bintang[0]
