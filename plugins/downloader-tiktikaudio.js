57c76e24b2cb5b8ca10cc82cd83f654cd3325c55
Burhan Syam <burhansyam85@gmail.com>
2022-09-22 14:15:20 +0700

    Update downloader-tiktikaudio.js

--- plugins/downloader-tiktikaudio.js
+++ plugins/downloader-tiktikaudio.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Contoh ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+if (!args[0]) throw `Contoh ${usedPrefix}${command} https://vt.tiktok.com/ZSR9Up4LR/`
 //     const { author: { nickname }, video, description } = await tiktokdl(args[0])
 //     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
 //     if (!url) throw 'Can\'t download video!'
@@ -9,7 +9,7 @@ if (!args[0]) throw `Contoh ${usedPrefix}${command} https://www.tiktok.com/@omag
     let res = await fetch(`https://hadi-api.herokuapp.com/api/tiktok?url=${args[0]}`)
     res = await res.json()
     let { audio1, audio2, original } = res.result.audio_only
-    conn.sendFile(m.chat, audio1, 'tiktok.mp3', `_©burhansyam_`.trim(), m)
+    conn.sendFile(m.chat, original, 'tiktok.mp3', `_©burhansyam_`.trim(), m)
 }
 handler.help = ['tiktokaudio'].map(v => v + ' <url>')
 handler.tags = ['downloader']
