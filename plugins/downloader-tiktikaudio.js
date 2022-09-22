c8aa0e7c6a9e85722ac26304bbab26b83841e095
Burhan Syam <burhansyam85@gmail.com>
2022-09-22 18:41:51 +0700

    Update downloader-tiktikaudio.js

--- plugins/downloader-tiktikaudio.js
+++ plugins/downloader-tiktikaudio.js
@@ -6,8 +6,11 @@ if (!args[0]) throw `Contoh ${usedPrefix}${command} https://vt.tiktok.com/ZSR9Up
     let res = await fetch(`http://hadi-api.herokuapp.com/api/tiktok?url=${args[0]}`)
     res = await res.json()
     let { original, audio1, audio2 } = res.result.audio_only
-    conn.sendFile(m.chat, original, 'tiktok.mp3', `_©burhansyam_`.trim(), m)
-// conn.sendButton(m.chat, `Download MP3 : ${original}`, 'Coded by @burhansyam', null, [['Server 2', `${audio1}`]], m)
+//     conn.sendFile(m.chat, original, 'tiktok.mp3', `_©burhansyam_`.trim(), m)
+let sound = await conn.getFile(`${original}`)
+		let caption = `*Tikcrot Sound Downloader*`
+                conn.sendFile(m.chat, sound.data, `${audio1}.mp3`, caption, m)    
+    
 
 }
 handler.help = ['tiktokaudio'].map(v => v + ' <url>')
